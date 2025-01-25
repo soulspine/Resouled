452a1dd3e01fdb1854f15d71a300918f2e8ebcf7
soulspine <soulspime@gmail.com>
2025-01-25 14:56:11 +0100

    fix Isaac's last will item deletion, add multi player support tweak EID description, fix no head after picking up devil deal bug

--- custom_scripts/items/isaacs_last_will.lua
+++ custom_scripts/items/isaacs_last_will.lua
@@ -3,17 +3,32 @@ ISAACS_LAST_WILL = Isaac.GetItemIdByName("Isaac's last will")
 print("Loaded Isaac's last will")
 
 local function onDeath(_)
-    local player = Isaac.GetPlayer()
-    if not Isaac.GetPlayer():HasCollectible(ISAACS_LAST_WILL) or player:WillPlayerRevive()
+    local game = Game()
+    local playerID = game:GetNumPlayers() - 1
+    ::checkAnotherPlayer::
+    if playerID < 0 -- no more players
     then
         return
     end
+    local player = Isaac.GetPlayer(playerID)
+    --print(GetTotalHP(player))
+    if not player:HasCollectible(ISAACS_LAST_WILL) or not player:IsDead() or player:WillPlayerRevive()
+    then
+        playerID = playerID - 1
+        goto checkAnotherPlayer
+    end
 
     player:RemoveCollectible(ISAACS_LAST_WILL)
-    player:AnimatePitfallOut()
     SFXManager():Play(SoundEffect.SOUND_LAZARUS_FLIP_ALIVE, 1, 10)
-    player:Revive()
 
+    -- DO NOT ANIMATE, REMOVES ISAAC'S HEAD UPON DYING AFTER TAKING A DEVIL DEAL
+    --player:AnimateSad()
+    
+    game:ShakeScreen(30)
+    game:Darken(1, 30)
+
+    player:Revive()
+    
     local guppyCount = player:HasPlayerForm(PlayerForm.PLAYERFORM_GUPPY)
     -- add guppy transformation
     -- its very sketchy, but it works
@@ -24,9 +39,9 @@ local function onDeath(_)
         for _ = 1, 4 do
             player:AddTrinket(TrinketType.TRINKET_KIDS_DRAWING, true)
         end
-
+        
         player:TryRemoveTrinket(TrinketType.TRINKET_KIDS_DRAWING)
-
+        
         if trinket1 ~= 0 then
             player:AddTrinket(trinket1, false)
         end
@@ -38,26 +53,43 @@ local function onDeath(_)
         end
     end
 
-    for _, itemId in pairs(CollectibleType) do
+    local pocketId = player:GetActiveItem(ActiveSlot.SLOT_POCKET)
+
+    local maxItemId = GetMaxItemID()
+
+    local checkCount = 1;
+    for itemId = 1, maxItemId do
+        if itemId == pocketId then --skip removing pocket for tainted
+            goto continue
+        end
         local itemCount = player:GetCollectibleNum(itemId)
+        if itemCount ~= 0 then
+            print("removing item: " .. itemId .. " count: " .. itemCount)
+        end
         for _ = 1, itemCount do
-            local rand = math.random(1, 2)
-            if rand ~= 1 then 
+            local currentStage = game:GetLevel():GetStage()
+            local stageSeed = game:GetSeeds():GetStageSeed(currentStage)
+            local rand = (stageSeed//((itemId*checkCount)+1)) % 2
+            print(rand)
+            if rand == 1 then --50% chance to remove item
                 player:RemoveCollectible(itemId)
             end
+            checkCount = checkCount + 1
         end
+        ::continue::
     end
     
-    player:SetPocketActiveItem(CollectibleType.COLLECTIBLE_GUPPYS_PAW, ActiveSlot.SLOT_POCKET, false)
-
-    --player:AddSoulHearts(1)
+    if pocketId == 0 then
+        player:SetPocketActiveItem(CollectibleType.COLLECTIBLE_GUPPYS_PAW, ActiveSlot.SLOT_POCKET, false)
+    end
+    
     player:AddMaxHearts(-player:GetMaxHearts())
     player:AddBoneHearts(-player:GetBoneHearts())
     player:AddBlackHearts(-player:GetBlackHearts())
     player:AddEternalHearts(-player:GetEternalHearts())
     player:AddGoldenHearts(-player:GetGoldenHearts())
     player:AddRottenHearts(-player:GetRottenHearts())
-
+    
     player:AddSoulHearts(4-player:GetSoulHearts())
 end
 MOD:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, onDeath, EntityType.ENTITY_PLAYER)

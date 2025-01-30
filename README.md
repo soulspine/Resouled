b6aa738d52ea36e85f7e45bd446fc5d71241e0ff
soulspine <soulspime@gmail.com>
2025-01-30 23:50:45 +0100

    add save manager

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Resouled
 Mod bringing Four Souls content into Isaac.
 
+# [Save Manager](https://github.com/catinsurance/IsaacSaveManager)
+by catinsurance \
+[docs](https://github.com/catinsurance/IsaacSaveManager/wiki)
+
 ## Credits
 - Isaac's last will death explosion code from [Emote Binds](https://steamcommunity.com/sharedfiles/filedetails/?id=3034946842) mod
--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 MOD = RegisterMod("Resouled", 1)
 
-local game = Game()
+include("custom_scripts.items")
+
+---@type SaveManager
+SAVE_MANAGER = include("custom_scripts.utility.save_manager")
+SAVE_MANAGER.Init(MOD)
 
 function GetMaxItemID()
     local itemConfig = Isaac.GetItemConfig()
@@ -16,15 +20,35 @@ function GetMaxItemID()
     return maxItemId - 1
 end
 
+---@param quality integer
+---@param rng RNG
+---@param position Vector
+---@param onlyUnlocks? boolean default true
+function MOD:SpawnItemOfQuality(quality, rng, position, onlyUnlocks)
+    onlyUnlocks = onlyUnlocks and true or false
+    local itemConfig = Isaac.GetItemConfig()
+    local validItems = {}
+    
+    for i = 1, #itemConfig:GetCollectibles() do
+        local item = itemConfig:GetCollectible(i)
+        if item and item.Quality == quality and not item.Hidden and (not onlyUnlocks or item:IsAvailable()) then
+            table.insert(validItems, i)
+        end
+    end
+    
+    if #validItems > 0 then
+        local randomItem = validItems[rng:RandomInt(#validItems) + 1]
+        return Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, randomItem, Isaac.GetFreeNearPosition(position, 60), Vector.Zero, nil)
+    end
+    return nil
+end
+
 ---@param callback function
 -- Iterates over all players in the game and calls the callback function with 2 first arguments: `player` and `playerID`.
 -- Passes all additional arguments to the callback function in the same order as they were passed to this function.
 function IterateOverPlayers(callback, ...)
-    for i = 0, game:GetNumPlayers() - 1 do
+    for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         callback(player, i, ...)
     end
 end
-
---import all item modules
-include("custom_scripts.items")

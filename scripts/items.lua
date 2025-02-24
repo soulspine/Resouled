38eba9b4b3422382db14dc0e212215120cf1449a
Mimi <mojeyt.dc@gmail.com>
2025-02-24 19:03:56 +0100

    Sort is now sorted by sets (except gfx and sfx)

--- scripts/items.lua
+++ scripts/items.lua
@@ -1,9 +1,11 @@
--- BASE GAME V2
-include("scripts.items.daddy_haunt")
-include("scripts.items.cheese_grater")
-
--- OTHERS
-include("scripts.items.isaacs_last_will")
-include("scripts.items.focus")
-include("scripts.items.strawberry")
-include("scripts.items.auction_gavel")
+-- Base Game V2
+include("scripts.items.BaseGameV2.daddy_haunt")
+include("scripts.items.BaseGameV2.cheese_grater")
+-- Four Souls V2
+-- Requiem
+include("scripts.items.Requiem.auction_gavel")
+-- Summer Of Isaac
+include("scripts.items.SummerOfIsaac.isaacs_last_will")
+-- Promotional Sets
+include("scripts.items.PromotionalSets.focus")
+include("scripts.items.PromotionalSets.strawberry")
--- scripts/pocketitems.lua
+++ scripts/pocketitems.lua
@@ -1,1 +1,6 @@
-include("scripts.pocketitems.chunk_of_amber")
+-- Base Game V2
+-- Four Souls V2
+-- Requiem
+-- Summer Of Isaac
+-- Promotional Sets
+include("scripts.pocketitems.PromotionalSets.chunk_of_amber")

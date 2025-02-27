5a932352f236599c55154f9fe123b1e149522876
soulspine <soulspime@gmail.com>
2025-02-27 11:15:59 +0100

    adjust cursed gaper to use halo giving method

--- scripts/enemies/BaseGameV2/cursed_gaper.lua
+++ scripts/enemies/BaseGameV2/cursed_gaper.lua
@@ -8,25 +8,7 @@ local HALO_SCALE = Vector(1.5, 1.5)
 ---@param npc EntityNPC
 local function onNpcInit(_, npc)
     if npc.Variant == CURSED_GAPER_VARIANT then
-        local entity = Game():Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HALO, npc.Position, Vector(0, 0), npc, HALO_SUBTYPE, 0)
-        local halo = entity:ToEffect()
-        halo.Parent = npc
-        halo.SpriteScale = HALO_SCALE
-        halo.ParentOffset = HALO_OFFSET
-        npc:GetData().Halo = halo
+        Resouled:AddHaloToNpc(npc, HALO_SUBTYPE, HALO_SCALE, HALO_OFFSET)
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_POST_NPC_INIT, onNpcInit, CURSED_GAPER_TYPE)
-
----@param npc EntityNPC
-local function preNpcUpdate(_, npc)
-    if npc.Variant == CURSED_GAPER_VARIANT then
-        ---@type EntityEffect
-        local halo = npc:GetData().Halo
-        if halo then
-            --halo.Position = npc.Position + HALO_OFFSET
-            halo.Position = npc.Position + HALO_OFFSET
-        end
-    end
-end
-Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, preNpcUpdate, CURSED_GAPER_TYPE)

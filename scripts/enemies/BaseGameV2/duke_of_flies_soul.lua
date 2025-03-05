8b0513048db5601e07904695ab49e940c34f73f3
Umimerhimhair <mojeyt.dc@gmail.com>
2025-03-05 21:28:27 +0100

    finish duke's soul bossfight, add haunt's soul sprite

--- scripts/enemies/BaseGameV2/duke_of_flies_soul.lua
+++ scripts/enemies/BaseGameV2/duke_of_flies_soul.lua
@@ -1,11 +1,15 @@
 local DUKE_OF_FLIES_SOUL_VARIANT = Isaac.GetEntityVariantByName("Duke's Soul")
 local DUKE_OF_FLIES_SOUL_ITEM_SUBTYPE = Isaac.GetItemIdByName("Duke's Soul")
 
+local DUKIE_SOUL_TYPE = Isaac.GetEntityTypeByName("Dukie's Soul")
+local DUKIE_SOUL_VARIANT = Isaac.GetEntityVariantByName("Dukie's Soul")
+
 local SPRITE_PLAYBACK_SPEED_MULTIPLIER = 1.7
 
 local SPRITE_SCALE_TO_ADD = 0.5
 local SPRITE_OFFSET = Vector(0, 10)
-local HITBOX_SIZE_MULTI = 1.5
+local HITBOX_SIZE_MULTI = Vector(1, 0.5)
+local HITBOX_SIZE_SCALE = 1.5
 
 local EVENT_TRIGGER_RESOULED_ATTACK1 = "ResouledAttack"
 local EVENT_TRIGGER_RESOULED_ATTACK2 = "ResouledAttack2"
@@ -13,16 +17,20 @@ local EVENT_TRIGGER_RESOULED_ATTACK_360 = "ResouledAttack3"
 
 local DUKIES_COUNT = 5
 
-local PROJECTILE_COUNT = 8
+local PROJECTILE_COUNT = 12
 local PROJECTILE_PARAMS = ProjectileParams()
-local PROJECTILE_FLAGS = (ProjectileFlags.BURST8)
+local PROJECTILE_SCALE = 1.5
+local PROJECTILE_VARIANT = 6
+local PROJECTILE_COLOR = Color(2, 5, 12.5, 0.5)
+local PROJECTILE_SPEED_MULTIPLIER = 12.5
 
 ---@param npc EntityNPC
 local function onNpcInit(_, npc)
     if npc.Variant == DUKE_OF_FLIES_SOUL_VARIANT then
         npc.Scale = npc.Scale + SPRITE_SCALE_TO_ADD --Sprite Scale
         npc.SpriteOffset = SPRITE_OFFSET --Sprite Offset
-        npc.Size = npc.Size * HITBOX_SIZE_MULTI --Hitbox Size
+        npc.Size = npc.Size * HITBOX_SIZE_SCALE --Hitbox Size
+        npc.SizeMulti = HITBOX_SIZE_MULTI
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_POST_NPC_INIT, onNpcInit, EntityType.ENTITY_DUKE)
@@ -34,18 +42,20 @@ local function onNpcUpdate(_, npc)
         sprite.PlaybackSpeed = SPRITE_PLAYBACK_SPEED_MULTIPLIER
         npc.Velocity = npc.Velocity * 1.15
 
+        
         if sprite:IsEventTriggered(EVENT_TRIGGER_RESOULED_ATTACK1) then
             for _ = 1, DUKIES_COUNT do
                 local DUKIES_POSITION = Vector(math.random(-75, 75), math.random(-75, 75))
-                local DUKIE = Game():Spawn(EntityType.ENTITY_DUKIE, 0, npc.Position + DUKIES_POSITION, Vector.Zero, npc, 0, npc.InitSeed)
+                Game():Spawn(DUKIE_SOUL_TYPE, DUKIE_SOUL_VARIANT, npc.Position + DUKIES_POSITION, Vector.Zero, npc, 0, npc.InitSeed)
             end
         end
-
+        
         if sprite:IsEventTriggered(EVENT_TRIGGER_RESOULED_ATTACK2) then
-            PROJECTILE_PARAMS.BulletFlags = PROJECTILE_FLAGS
-            --npc:FireBossProjectiles(5, npc:GetPlayerTarget().Position, 1, PROJECTILE_PARAMS)
+            PROJECTILE_PARAMS.Scale = PROJECTILE_SCALE
+            PROJECTILE_PARAMS.Color = PROJECTILE_COLOR
+            PROJECTILE_PARAMS.Variant = PROJECTILE_VARIANT
             for i = 1, PROJECTILE_COUNT do
-                npc:FireProjectiles(npc.Position, Vector.FromAngle(i * 360 / PROJECTILE_COUNT):Resized(1)*10, 0, PROJECTILE_PARAMS)
+                npc:FireProjectiles(npc.Position, Vector.FromAngle(i * 360 / PROJECTILE_COUNT):Resized(1)*PROJECTILE_SPEED_MULTIPLIER, 0, PROJECTILE_PARAMS)
             end
         end
     end

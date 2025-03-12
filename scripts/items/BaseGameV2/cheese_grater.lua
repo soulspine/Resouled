ba54b2399f1aa3d9cc72a7c06f563c86f87116e3
soulspine <soulspime@gmail.com>
2025-03-12 14:35:06 +0100

    add "grating off" tear effect to cheese grater

--- scripts/items/BaseGameV2/cheese_grater.lua
+++ scripts/items/BaseGameV2/cheese_grater.lua
@@ -1,11 +1,63 @@
 local CHEESE_GRATER = Isaac.GetItemIdByName("Cheese Grater")
 
+local DAMAGE_MULTIPLIER = 2.5
+local COOLDOWN = 100
+
+local GRATED_OFF_ENEMY_HEALTH_FRACTION = 0.01
+local GRATED_OFF_ENEMY_TYPE = EntityType.ENTITY_SMALL_MAGGOT
+local GRATED_OFF_ENEMY_VARIANT = 0
+local GRATED_OFF_ENEMY_SUBTYPE = 0
+
 if EID then
     EID:addCollectible(CHEESE_GRATER, "Reveals all {{QuestionMark}} question mark items.#Works on alt path choices and {{CurseBlind}} Curse of the Blind.", "Cheese Grater")
 end
 
+---@param luck number
+---@return number
+local APPLY_TEAR_EFFECT_CHANCE = function(luck)
+    return ((6 + luck) / 18)^2
+end
+
+---@param tear EntityTear
+local function onTearInit(_, tear)
+    local player = tear.SpawnerEntity:ToPlayer()
+    if player and player:HasCollectible(CHEESE_GRATER) and tear:GetDropRNG():RandomFloat() < APPLY_TEAR_EFFECT_CHANCE(player.Luck) then
+        Resouled:ApplyCustomTearEffect(tear, Resouled.TearEffects.CHEESE_GRATER)
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_POST_TEAR_INIT, onTearInit)
+
+---@param entity Entity
+---@param amount number
+---@param damageFlag integer
+---@param source EntityRef
+---@param countdown integer
+local function onEntityTakeDamage(_, entity, amount, damageFlag, source, countdown)
+    if source.Entity then
+        local player = source.Entity:ToPlayer()
+        local tear = source.Entity:ToTear()
+        local npc = entity:ToNPC()
+        if
+        (player and player:HasCollectible(CHEESE_GRATER) and player:GetCollectibleRNG(CHEESE_GRATER):RandomFloat() < APPLY_TEAR_EFFECT_CHANCE(player.Luck)) or
+        (tear and Resouled:GetCustomTearEffects(tear) and Resouled:GetCustomTearEffects(tear) | Resouled.TearEffects.CHEESE_GRATER == Resouled.TearEffects.CHEESE_GRATER) then
+            if npc and npc:IsVulnerableEnemy() and not (damageFlag & DamageFlag.DAMAGE_FAKE == DamageFlag.DAMAGE_FAKE) and not Resouled:IsCustomTearEffectOnCooldown(npc, Resouled.TearEffects.CHEESE_GRATER) then
+                entity:TakeDamage(DAMAGE_MULTIPLIER * amount, damageFlag | DamageFlag.DAMAGE_FAKE, source, countdown)
+                Resouled:ApplyCustomTearEffectCooldown(npc, Resouled.TearEffects.CHEESE_GRATER, COOLDOWN)
+
+                if npc.HitPoints - DAMAGE_MULTIPLIER * amount > 0 then
+                    local gratedOffEntity = Game():Spawn(GRATED_OFF_ENEMY_TYPE, GRATED_OFF_ENEMY_VARIANT, npc.Position, Vector.FromAngle(math.random(360)):Normalized(), npc, GRATED_OFF_ENEMY_SUBTYPE, npc.DropSeed)
+                    gratedOffEntity:AddHealth(-1*(1-GRATED_OFF_ENEMY_HEALTH_FRACTION)*gratedOffEntity.MaxHitPoints)
+                end
+
+                return false
+            end
+        end
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, onEntityTakeDamage)
+
 ---@param pickup EntityPickup
-local function oncCollectibleUpdate(_, pickup) 
+local function oncCollectibleUpdate(_, pickup)
 -- THIS HAS TO BE POST UPDATE BECAUSE POST INIT THERE IS NO SPRITE LOADED YET
 -- POST RENDER ALSO DOES NOT WORK CORRECTLY BECAUSE IT GOES BACK AND FORTH BETWEEN BEING A QUESTION MARK AND NOT
     if Resouled:CollectiblePresent(CHEESE_GRATER) then
--- scripts/utility/resouled.lua
+++ scripts/utility/resouled.lua
@@ -1,3 +1,8 @@
+---@enum ResouledTearEffects
+Resouled.TearEffects = {
+    CHEESE_GRATER = 0
+}
+
 -- Iterates over all players in the game and calls the callback function with first argument being `player`.
 -- Passes all additional arguments to the callback function in the same order as they were passed to this function.
 ---@param callback function
@@ -470,3 +475,61 @@ function Resouled:TryRevealQuestionMarkItem(pickup)
         return false
     end
 end
+
+--- Applies a custom tear effect to the tear entity
+--- @param tear EntityTear | EntityLaser
+--- @param effect ResouledTearEffects
+function Resouled:ApplyCustomTearEffect(tear, effect)
+    local data = tear:GetData()
+    if data.ResouledTearEffect then
+        data.ResouledTearEffect = data.ResouledTearEffect | effect
+    else
+        data.ResouledTearEffect = effect
+    end
+end
+
+--- Returns an bitmask representing custom tear effects applied to the tear entity
+--- @param tear EntityTear | EntityLaser
+function Resouled:GetCustomTearEffects(tear)
+    return tear:GetData().ResouledTearEffect
+end
+
+--- Applies a cooldown so that the custom tear effect can't be applied again for the specified duration
+--- @param npc EntityNPC
+--- @param effect ResouledTearEffects
+--- @param duration integer
+function Resouled:ApplyCustomTearEffectCooldown(npc, effect, duration)
+    local data = npc:GetData()
+    if data.ResouledTearEffectCooldown then
+        data.ResouledTearEffectCooldown[effect] = duration
+    else
+        data.ResouledTearEffectCooldown = {
+            [effect] = duration
+        }
+    end
+end
+
+--- Returns whether the custom tear effect is on cooldown
+--- @param npc EntityNPC
+--- @param effect ResouledTearEffects
+--- @return boolean
+function Resouled:IsCustomTearEffectOnCooldown(npc, effect)
+    local data = npc:GetData()
+    if data.ResouledTearEffectCooldown then
+        return data.ResouledTearEffectCooldown[effect] > 0
+    else
+        return false
+    end
+end
+
+---@param npc EntityNPC
+Resouled:AddCallback(ModCallbacks.MC_NPC_UPDATE, function(_, npc)
+    local data = npc:GetData()
+    if data.ResouledTearEffectCooldown then
+        for effect, cooldown in pairs(data.ResouledTearEffectCooldown) do
+            if cooldown > 0 then
+                data.ResouledTearEffectCooldown[effect] = cooldown - 1
+            end
+        end
+    end
+end)

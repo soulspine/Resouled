859aa7354b8e142369e471c52254f51020bf8c00
Umimerhimhair <mojeyt.dc@gmail.com>
2025-03-12 22:33:35 +0100

    add cursed psy horf (the shoot trigger is triggered a milion times for some reason)

---
+++ scripts/enemies/BaseGameV2/cursed_psy_horf.lua
@@ -0,0 +1,45 @@
+local CURSED_PSY_HORF_VARIANT = Isaac.GetEntityVariantByName("Cursed Psy Horf")
+local CURSED_PSY_HORF_TYPE = Isaac.GetEntityTypeByName("Cursed Psy Horf")
+local HALO_SUBTYPE = 3
+
+local HALO_OFFSET = Vector(0, -15)
+local HALO_SCALE = Vector(1.5, 1.5)
+
+local ACTIVATION_DISTANCE = 110
+
+local CURSED_ENEMY_MORPH_CHANCE = 0.1
+
+local SHOOT = "ResouledShoot"
+
+local PROJECTILE_PARAMS = ProjectileParams()
+local PROJECTILE_FLAGS = (ProjectileFlags.SMART)
+
+---@param npc EntityNPC
+local function onNpcInit(_, npc)
+    --Try to turn enemy into a cursed enemy
+    if Game():GetLevel():GetCurses() > 0 then
+        Resouled:TryEnemyMorph(npc, CURSED_ENEMY_MORPH_CHANCE, CURSED_PSY_HORF_TYPE, CURSED_PSY_HORF_VARIANT, 0)
+    end
+    
+
+    --Add halo
+    if npc.Variant == CURSED_PSY_HORF_VARIANT then
+        local data = npc:GetData()
+        PROJECTILE_PARAMS.BulletFlags = PROJECTILE_FLAGS
+        data.ProjectileParams = PROJECTILE_PARAMS
+        Resouled:AddHaloToNpc(npc, HALO_SUBTYPE, HALO_SCALE, HALO_OFFSET)
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_POST_NPC_INIT, onNpcInit, CURSED_PSY_HORF_TYPE)
+
+---@param npc EntityNPC
+local function preNpcUpdate(_, npc)
+    if npc.Variant == CURSED_PSY_HORF_VARIANT then
+        local sprite = npc:GetSprite()
+        local data = npc:GetData()
+        if sprite:WasEventTriggered(SHOOT) then
+            npc:FireProjectiles(npc.Position, (npc:GetPlayerTarget().Position - npc.Position) / 25, 0, data.ProjectileParams)
+        end
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, preNpcUpdate, CURSED_PSY_HORF_TYPE)
--- scripts/enemies/BaseGameV2/wraths_soul.lua
+++ scripts/enemies/BaseGameV2/wraths_soul.lua
@@ -5,7 +5,7 @@ local WRATHS_SOUL_SUBTYPE = 0
 ---@param npc EntityNPC
 local function onNpcUpdate(_, npc)
     if npc.Variant == WRATHS_SOUL_VARIANT and npc.SubType == WRATHS_SOUL_SUBTYPE then
-        
+
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_NPC_UPDATE, onNpcUpdate, WRATHS_SOUL_TYPE)

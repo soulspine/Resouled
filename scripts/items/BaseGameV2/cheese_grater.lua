42ed2e921f1d5fe5bc0f243925a6d78ad0de912f
soulspine <soulspime@gmail.com>
2025-03-19 04:42:31 +0100

    fix a bug where cheese grater would not allow to kill spawned maggots

--- scripts/items/BaseGameV2/cheese_grater.lua
+++ scripts/items/BaseGameV2/cheese_grater.lua
@@ -3,6 +3,10 @@ local CHEESE_GRATER = Isaac.GetItemIdByName("Cheese Grater")
 local DAMAGE_MULTIPLIER = 2.5
 local COOLDOWN = 100
 
+local SPECIAL_TEAR_COLOR = Color(1, 0.6, 1, 0.7, 0.7, 0.7, 0)
+local SPECIAL_TEAR_COLOR_DURATION = 300
+local SPECIAL_TEAR_COLOR_PRIORITY = 1
+
 local GRATED_OFF_ENEMY_HEALTH_FRACTION = 0.01
 local GRATED_OFF_ENEMY_TYPE = EntityType.ENTITY_SMALL_MAGGOT
 local GRATED_OFF_ENEMY_VARIANT = 0
@@ -10,7 +14,7 @@ local GRATED_OFF_ENEMY_SUBTYPE = 0
 local GRATED_OFF_ENEMY_SPAWN_VELOCITY_MULTIPLIER = 3
 
 if EID then
-    EID:addCollectible(CHEESE_GRATER, "Reveals all {{QuestionMark}} question mark items.#Works on alt path choices and {{CurseBlind}} Curse of the Blind.", "Cheese Grater")
+    EID:addCollectible(CHEESE_GRATER, "Adds a luck based chance to shoot a special tear that deals " .. DAMAGE_MULTIPLIER .. "x damage but spawns a leech with 1HP.#Reveals all {{QuestionMark}} question mark items.", "Cheese Grater")
 end
 
 ---@param luck number
@@ -24,6 +28,7 @@ local function onTearInit(_, tear)
     local player = tear.SpawnerEntity:ToPlayer()
     if player and player:HasCollectible(CHEESE_GRATER) and tear:GetDropRNG():RandomFloat() < APPLY_TEAR_EFFECT_CHANCE(player.Luck) then
         Resouled:ApplyCustomTearEffect(tear, Resouled.TearEffects.CHEESE_GRATER)
+        tear:SetColor(SPECIAL_TEAR_COLOR, SPECIAL_TEAR_COLOR_DURATION, SPECIAL_TEAR_COLOR_PRIORITY, true, false)
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_POST_TEAR_INIT, onTearInit)
@@ -44,21 +49,24 @@ local function onEntityTakeDamage(_, entity, amount, damageFlag, source, countdo
         end
 
         local npc = entity:ToNPC()
+        local npcData = entity:GetData()
         if
         (player and player:HasCollectible(CHEESE_GRATER) and player:GetCollectibleRNG(CHEESE_GRATER):RandomFloat() < APPLY_TEAR_EFFECT_CHANCE(player.Luck)) or
         (tear and Resouled:GetCustomTearEffects(tear) and Resouled:GetCustomTearEffects(tear) | Resouled.TearEffects.CHEESE_GRATER == Resouled.TearEffects.CHEESE_GRATER) then
-            if npc and npc:IsVulnerableEnemy() and not (damageFlag & DamageFlag.DAMAGE_FAKE == DamageFlag.DAMAGE_FAKE) and not Resouled:IsCustomTearEffectOnCooldown(npc, Resouled.TearEffects.CHEESE_GRATER) then
+            if npc and npc:IsVulnerableEnemy() and not npcData.CheeseGraterMultiDamage and not Resouled:IsCustomTearEffectOnCooldown(npc, Resouled.TearEffects.CHEESE_GRATER) then
 
                 local newAmount = DAMAGE_MULTIPLIER * amount
                 if npc.HitPoints - newAmount > 0 then
-                    entity:TakeDamage(newAmount, damageFlag | DamageFlag.DAMAGE_FAKE, source, countdown)
+                    npcData.CheeseGraterMultiDamage = true
+                    entity:TakeDamage(newAmount, damageFlag, source, countdown)
                     Resouled:ApplyCustomTearEffectCooldown(npc, Resouled.TearEffects.CHEESE_GRATER, COOLDOWN)
 
                     local gratedOffEntity = Game():Spawn(GRATED_OFF_ENEMY_TYPE, GRATED_OFF_ENEMY_VARIANT, npc.Position, Vector.FromAngle(math.random(360)):Normalized() * GRATED_OFF_ENEMY_SPAWN_VELOCITY_MULTIPLIER, npc, GRATED_OFF_ENEMY_SUBTYPE, npc.DropSeed)
                     gratedOffEntity:AddHealth(-1*(1-GRATED_OFF_ENEMY_HEALTH_FRACTION)*gratedOffEntity.MaxHitPoints)
+                    return false
                 end
-
-                return false
+            else
+                npcData.CheeseGraterMultiDamage = false
             end
         end
     end

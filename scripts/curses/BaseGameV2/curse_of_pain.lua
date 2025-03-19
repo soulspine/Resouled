addd4a5cf6967b3e16a3ec690f5c837a2d4b9522
soulspine <soulspime@gmail.com>
2025-03-19 03:05:56 +0100

    make curse of pain works as damage source instead of heart removal

--- scripts/curses/BaseGameV2/curse_of_pain.lua
+++ scripts/curses/BaseGameV2/curse_of_pain.lua
@@ -4,17 +4,15 @@
 ---@param source EntityRef
 ---@param countdown integer
 local function onPlayerHit(_, entity, amount, flags, source, countdown)
-    local player = entity:ToPlayer()
-    if not entity:ToPlayer():HasCollectible(CollectibleType.COLLECTIBLE_WAFER) then
-        if player and Resouled:CustomCursePresent(Resouled.Curses.CURSE_OF_PAIN) and flags & DamageFlag.DAMAGE_FAKE == 0 then
-            if Resouled:GetEffectiveBlackHP(player) > 0 then
-                player:AddBlackHearts(-1)
-            elseif Resouled:GetEffectiveBlackHP(player) <= 0 and Resouled:GetEffectiveSoulHP(player) > 0 then
-                player:AddSoulHearts(-1)
-            elseif Resouled:GetEffectiveBlackHP(player) <= 0 and Resouled:GetEffectiveSoulHP(player) <= 0 and Resouled:GetEffectiveRedHP(player) > 0 then
-                player:AddHearts(-1)
-            end
-        end
+    local entityData = entity:GetData()
+    if entity.Type == EntityType.ENTITY_PLAYER and Resouled:CustomCursePresent(Resouled.Curses.CURSE_OF_PAIN) and amount == 1 and not entityData.CurseOfPain then
+        entityData.CurseOfPain = true
+        entity:TakeDamage(2, flags, source, countdown)
+        return false
+    end
+
+    if entityData.CurseOfPain then
+        entityData.CurseOfPain = false
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, onPlayerHit, EntityType.ENTITY_PLAYER)

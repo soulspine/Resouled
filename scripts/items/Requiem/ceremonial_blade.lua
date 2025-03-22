bffdfa3ecde661d4d876ceabd793152f73cc5692
soulspine <soulspime@gmail.com>
2025-03-22 20:45:15 +0100

    make pickups spawned by ceremonial blade consistent based on seed

--- scripts/items/Requiem/ceremonial_blade.lua
+++ scripts/items/Requiem/ceremonial_blade.lua
@@ -1,8 +1,31 @@
 local CEREMONIAL_BLADE = Isaac.GetItemIdByName("Ceremonial Blade")
 
-local ROOM_CLEAR_EFFECT_VARIANT = EffectVariant.HERETIC_PENTAGRAM
-local ROOM_CLEAR_EFFECT_TIMEOUT = 1
-local ROOM_CLEAR_EFFECT_SCALE = Vector(0.15, 0.15)
+local ROOM_CLEAR_FADING_PICKUP_VELOCITY = 2
+local ROOM_CLEAR_FADING_PICKUP_TIMEOUT = 60
+local ROOM_CLEAR_WHITELISTED_PICKUPS = {
+    PickupVariant.PICKUP_HEART,
+    PickupVariant.PICKUP_COIN,
+    PickupVariant.PICKUP_KEY,
+    PickupVariant.PICKUP_BOMB,
+    PickupVariant.PICKUP_POOP,
+    PickupVariant.PICKUP_CHEST,
+    PickupVariant.PICKUP_BOMBCHEST,
+    PickupVariant.PICKUP_SPIKEDCHEST,
+    PickupVariant.PICKUP_ETERNALCHEST,
+    PickupVariant.PICKUP_MIMICCHEST,
+    PickupVariant.PICKUP_OLDCHEST,
+    PickupVariant.PICKUP_WOODENCHEST,
+    PickupVariant.PICKUP_MEGACHEST,
+    PickupVariant.PICKUP_LOCKEDCHEST,
+    PickupVariant.PICKUP_GRAB_BAG,
+    PickupVariant.PICKUP_LIL_BATTERY,
+    PickupVariant.PICKUP_TAROTCARD,
+    PickupVariant.PICKUP_REDCHEST,
+}
+
+local PICKUP_EFFECT_VARIANT = EffectVariant.HERETIC_PENTAGRAM
+local PICKUP_EFFECT_TIMEOUT = 1
+local PICKUP_EFFECT_SCALE = Vector(0.15, 0.15)
 
 local ENEMY_KILL_BASE_PICKUP_DROP_CHANCE = 0.05
 local ENEMY_KILL_PICKUP_DROP_CHANCE_PER_1_LUCK = 0.0025
@@ -13,7 +36,19 @@ local ENEMY_KILL_WHITELISTED_PICKUPS = {
     PickupVariant.PICKUP_KEY,
 }
 
+if EID then
+    EID:addCollectible(CEREMONIAL_BLADE, "Luck based chance to drop a {{Coin}} coin / {{Bomb}} bomb / {{Key}} key when killing an enemy.#On room clear makes Isaac drop 1 pickup with no way to pick it up and spawns a new random pickup.", "Ceremonial Blade")
+end
 
+---@param spawner Entity
+local function spawnEffect(spawner)
+    local effect = Game():Spawn(EntityType.ENTITY_EFFECT, PICKUP_EFFECT_VARIANT, spawner.Position, Vector.Zero, spawner, 0, 0):ToEffect()
+    if effect then
+        effect:SetTimeout(PICKUP_EFFECT_TIMEOUT)
+        effect:GetSprite().Scale = PICKUP_EFFECT_SCALE
+        effect:GetData().ResouledCeremonialBladeEffect = true
+    end
+end
 ---@param npc EntityNPC
 local function onNpcDeath(_, npc)
     local playerLuck = 0
@@ -38,7 +73,10 @@ local function onNpcDeath(_, npc)
                         newPickupSeed = Random()
                     end
 
-                    Game():Spawn(EntityType.ENTITY_PICKUP, ENEMY_KILL_WHITELISTED_PICKUPS[rng:RandomInt(#ENEMY_KILL_WHITELISTED_PICKUPS) + 1], npc.Position, Vector(math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY), math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY)), nil, 1, newPickupSeed)
+                    local pickup = Game():Spawn(EntityType.ENTITY_PICKUP, ENEMY_KILL_WHITELISTED_PICKUPS[rng:RandomInt(#ENEMY_KILL_WHITELISTED_PICKUPS) + 1], npc.Position, Vector(math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY), math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY)), nil, 1, newPickupSeed):ToPickup()
+                    if pickup then
+                        spawnEffect(pickup)
+                    end
                 end
             end
         end
@@ -86,15 +124,37 @@ local function onRoomClear()
     
             local spawnPos = room:GetCenterPos()
     
-            local pickup = Game():Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_NULL, spawnPos, Vector.Zero, nil, 0, newPickupSeed):ToPickup()
+            local fadingPickup = Game():Spawn(EntityType.ENTITY_PICKUP, pickupToRemove, player0.Position, Vector(math.random(-ROOM_CLEAR_FADING_PICKUP_VELOCITY, ROOM_CLEAR_FADING_PICKUP_VELOCITY), math.random(-ROOM_CLEAR_FADING_PICKUP_VELOCITY,ROOM_CLEAR_FADING_PICKUP_VELOCITY)), nil, 1, newPickupSeed):ToPickup()
+
+            if fadingPickup then
+                fadingPickup.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
+                fadingPickup.Timeout = ROOM_CLEAR_FADING_PICKUP_TIMEOUT
+                fadingPickup:GetData().ResouledCeremonialBladeCancelCollision = true
+            end
+
+            local pickup = Game():Spawn(EntityType.ENTITY_PICKUP, ROOM_CLEAR_WHITELISTED_PICKUPS[roomRng:RandomInt(#ROOM_CLEAR_WHITELISTED_PICKUPS) + 1], room:FindFreePickupSpawnPosition(spawnPos), Vector.Zero, nil, 0, newPickupSeed):ToPickup()
             if pickup then
-                local effect = Game():Spawn(EntityType.ENTITY_EFFECT, ROOM_CLEAR_EFFECT_VARIANT, pickup.Position, Vector.Zero, nil, 0, 0):ToEffect()
-                if effect then
-                    effect:SetTimeout(ROOM_CLEAR_EFFECT_TIMEOUT)
-                    effect:GetSprite().Scale = ROOM_CLEAR_EFFECT_SCALE
-                end
+                spawnEffect(pickup)
             end        
         end
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, onRoomClear)
+
+---@param pickup EntityPickup
+---@param collider Entity
+---@param low boolean
+local function onPickupCollision(_, pickup, collider, low)
+    if pickup:GetData().ResouledCeremonialBladeCancelCollision then
+        return false
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, onPickupCollision)
+
+---@param effect EntityEffect
+local function onEffectUpdate(_, effect)
+    if effect:GetData().ResouledCeremonialBladeEffect and effect.SpawnerEntity then
+        effect.Position = effect.SpawnerEntity.Position
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, onEffectUpdate)

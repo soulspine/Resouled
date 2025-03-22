c2230c0312f9c83ec8344d090a3fe50f25ffba9f
soulspine <soulspime@gmail.com>
2025-03-22 19:50:08 +0100

    add effect to ceremonial blade and clean up the code

--- scripts/items/Requiem/ceremonial_blade.lua
+++ scripts/items/Requiem/ceremonial_blade.lua
@@ -1,7 +1,18 @@
 local CEREMONIAL_BLADE = Isaac.GetItemIdByName("Ceremonial Blade")
-local BASE_PICKUP_DROP_CHANCE = 0.05
-local PICKUP_DROP_CHANCE_PER_1_LUCK = 0.0025
-local PICKUP_VELOCITY = 2
+
+local ROOM_CLEAR_EFFECT_VARIANT = EffectVariant.HERETIC_PENTAGRAM
+local ROOM_CLEAR_EFFECT_TIMEOUT = 1
+local ROOM_CLEAR_EFFECT_SCALE = Vector(0.15, 0.15)
+
+local ENEMY_KILL_BASE_PICKUP_DROP_CHANCE = 0.05
+local ENEMY_KILL_PICKUP_DROP_CHANCE_PER_1_LUCK = 0.0025
+local ENEMY_KILL_PICKUP_VELOCITY = 2
+local ENEMY_KILL_WHITELISTED_PICKUPS = {
+    PickupVariant.PICKUP_COIN,
+    PickupVariant.PICKUP_BOMB,
+    PickupVariant.PICKUP_KEY,
+}
+
 
 ---@param npc EntityNPC
 local function onNpcDeath(_, npc)
@@ -20,17 +31,15 @@ local function onNpcDeath(_, npc)
             local rng = RNG()
             rng:SetSeed(npc.DropSeed, 0)
             for _ = 1, ceremonialBladeAmount do
-                local chance = rng:RandomFloat()
-                if chance <= BASE_PICKUP_DROP_CHANCE + (PICKUP_DROP_CHANCE_PER_1_LUCK * playerLuck) then
-                    local pickup = rng:RandomInt(3)
-                    local pickupTranslation = {
-                        [0] = PickupVariant.PICKUP_COIN,
-                        [1] = PickupVariant.PICKUP_BOMB,
-                        [2] = PickupVariant.PICKUP_KEY,
-                    }
-                    Game():Spawn(EntityType.ENTITY_PICKUP, pickupTranslation[pickup], npc.Position, Vector(math.random(-PICKUP_VELOCITY, PICKUP_VELOCITY) + math.random(-1, 1), math.random(-PICKUP_VELOCITY, PICKUP_VELOCITY) + math.random(-1, 1)), nil, 1, rng:GetSeed())
+                if rng:RandomFloat() <= ENEMY_KILL_BASE_PICKUP_DROP_CHANCE + (ENEMY_KILL_PICKUP_DROP_CHANCE_PER_1_LUCK * playerLuck) then
+
+                    local newPickupSeed = 0
+                    while newPickupSeed == 0 do
+                        newPickupSeed = Random()
+                    end
+
+                    Game():Spawn(EntityType.ENTITY_PICKUP, ENEMY_KILL_WHITELISTED_PICKUPS[rng:RandomInt(#ENEMY_KILL_WHITELISTED_PICKUPS) + 1], npc.Position, Vector(math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY), math.random(-ENEMY_KILL_PICKUP_VELOCITY, ENEMY_KILL_PICKUP_VELOCITY)), nil, 1, newPickupSeed)
                 end
-                rng:Next()
             end
         end
     end
@@ -38,40 +47,53 @@ end
 Resouled:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, onNpcDeath)
 
 local function onRoomClear()
-    local ceremonialBladeAmount = 0
-    ---@param player EntityPlayer
-    Resouled:IterateOverPlayers(function(player)
-        if player:HasCollectible(CEREMONIAL_BLADE) then
-            ceremonialBladeAmount = ceremonialBladeAmount + player:GetCollectibleNum(CEREMONIAL_BLADE)
+    if Resouled:CollectiblePresent(CEREMONIAL_BLADE) then
+        local room = Game():GetRoom()
+        local roomRng = RNG()
+        roomRng:SetSeed(room:GetAwardSeed(), 4) -- random 4 shift because why not
+    
+        local player0 = Isaac.GetPlayer(0)
+        local allowedPickupsToRemove = {}
+    
+        if player0:GetNumCoins() > 0 then
+            table.insert(allowedPickupsToRemove, PickupVariant.PICKUP_COIN)
         end
-    end)
-    if ceremonialBladeAmount > 0 then
-        local player = Isaac.GetPlayer()
-        if player:GetNumCoins() > 0 and player:GetNumBombs() > 0 and player:GetNumKeys() > 0 then
-            for _ = 1, ceremonialBladeAmount do
-                local rng = RNG()
-                rng:SetSeed(Game():GetRoom():GetSeededCollectible(Game():GetRoom():GetAwardSeed(), false), 0)
-                local pickupToAdd = rng:RandomInt(3)
-                rng:Next()
-                local coin = 0
-                local bomb = 1
-                local key = 2
-                local pickupToRemove = rng:RandomInt(3)
-                if pickupToAdd == coin then
-                    player:AddCoins(1)
-                elseif pickupToAdd == bomb then
-                    player:AddBombs(1)
-                elseif pickupToAdd == key then
-                    player:AddKeys(1)
-                end
-                if pickupToRemove == coin then
-                    player:AddCoins(-1)
-                elseif pickupToRemove == bomb then
-                    player:AddBombs(-1)
-                elseif pickupToRemove == key then
-                    player:AddKeys(-1)
-                end
+    
+        if player0:GetNumBombs() > 0 then
+            table.insert(allowedPickupsToRemove, PickupVariant.PICKUP_BOMB)
+        end
+    
+        if player0:GetNumKeys() > 0 then
+            table.insert(allowedPickupsToRemove, PickupVariant.PICKUP_KEY)
+        end
+    
+        if #allowedPickupsToRemove > 0 then
+            local pickupToRemove = allowedPickupsToRemove[roomRng:RandomInt(#allowedPickupsToRemove) + 1]
+    
+            if pickupToRemove == PickupVariant.PICKUP_COIN then
+                player0:AddCoins(-1)
+            elseif pickupToRemove == PickupVariant.PICKUP_BOMB then
+                player0:AddBombs(-1)
+            elseif pickupToRemove == PickupVariant.PICKUP_KEY then
+                player0:AddKeys(-1)
             end
+    
+            local newPickupSeed = 0
+    
+            while newPickupSeed == 0 do
+                newPickupSeed = Random()
+            end
+    
+            local spawnPos = room:GetCenterPos()
+    
+            local pickup = Game():Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_NULL, spawnPos, Vector.Zero, nil, 0, newPickupSeed):ToPickup()
+            if pickup then
+                local effect = Game():Spawn(EntityType.ENTITY_EFFECT, ROOM_CLEAR_EFFECT_VARIANT, pickup.Position, Vector.Zero, nil, 0, 0):ToEffect()
+                if effect then
+                    effect:SetTimeout(ROOM_CLEAR_EFFECT_TIMEOUT)
+                    effect:GetSprite().Scale = ROOM_CLEAR_EFFECT_SCALE
+                end
+            end        
         end
     end
 end

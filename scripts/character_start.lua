6ed55bc5a94521c601bdcad40095b65939be2fbf
soulspine <soulspime@gmail.com>
2025-03-27 20:21:31 +0100

    add a variable to character start

--- scripts/character_start.lua
+++ scripts/character_start.lua
@@ -7,14 +7,15 @@ local SIBLING_RIVALRY = Isaac.GetItemIdByName("Sibling Rivalry")
 
 ---@param player EntityPlayer
 local function onPlayerInit(_, player)
+    local playerType = player:GetPlayerType()
     --BaseGameV2
-    if player:GetPlayerType() == PlayerType.PLAYER_CAIN then
+    if playerType == PlayerType.PLAYER_CAIN then
         if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) == CollectibleType.COLLECTIBLE_NULL then
             player:AddCollectible(SLEIGHT_OF_HAND, 0, true, ActiveSlot.SLOT_PRIMARY, 0)
         end
     end
 
-    if player:GetPlayerType() == PlayerType.PLAYER_BLUEBABY then
+    if playerType == PlayerType.PLAYER_BLUEBABY then
         player:AddCollectible(FOREVER_ALONE)
         player:AddCacheFlags(CacheFlag.CACHE_FAMILIARS)
         player:CheckFamiliar(FamiliarVariant.FOREVER_ALONE, 1, player:GetCollectibleRNG(FOREVER_ALONE))
@@ -22,23 +23,23 @@ local function onPlayerInit(_, player)
     --FourSoulsV2
 
     --Requiem
-    if player:GetPlayerType() == PlayerType.PLAYER_JACOB then
+    if playerType == PlayerType.PLAYER_JACOB then
         if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) == CollectibleType.COLLECTIBLE_NULL then
             player:AddCollectible(SIBLING_RIVALRY, 0, true, ActiveSlot.SLOT_PRIMARY, 0)
         end
     end
 
-    if player:GetPlayerType() == PlayerType.PLAYER_ISAAC_B then
+    if playerType == PlayerType.PLAYER_ISAAC_B then
         if player:GetActiveItem(ActiveSlot.SLOT_POCKET) == CollectibleType.COLLECTIBLE_NULL then
             player:SetPocketActiveItem(SPINDOWN_DICE, ActiveSlot.SLOT_POCKET, false)
         end
     end
 
-    if player:GetPlayerType() == PlayerType.PLAYER_JUDAS_B then
+    if playerType == PlayerType.PLAYER_JUDAS_B then
         player:AddCollectible(CEREMONIAL_BLADE)
     end
     
-    if player:GetPlayerType() == PlayerType.PLAYER_KEEPER_B then
+    if playerType == PlayerType.PLAYER_KEEPER_B then
         player:AddTrinket(KEEPERS_BARGAIN, true)
     end
 end

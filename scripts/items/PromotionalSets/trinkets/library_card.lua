3bc68a63dd47ae0663fb816ed9bc3daa70bea371
Umimerhimhair <mojeyt.dc@gmail.com>
2025-03-29 01:11:25 +0100

    finish library card

--- scripts/items/PromotionalSets/trinkets/library_card.lua
+++ scripts/items/PromotionalSets/trinkets/library_card.lua
@@ -1,30 +1,35 @@
 local LIBRARY_CARD = Isaac.GetTrinketIdByName("Library Card")
 
+local BLACKLISTED_STAGES = {
+    [LevelStage.STAGE4_3] = true,
+    [LevelStage.STAGE8] = true
+}
+
 local function onNewFloor()
-    local libraryCardPresent = false
-    ---@param player EntityPlayer
-    Resouled:IterateOverPlayers(function(player)
-        if player:HasTrinket(LIBRARY_CARD) then
-            libraryCardPresent = true
-        end
-    end)
-    local seed = 1
-    if libraryCardPresent and Game():GetLevel() ~= LevelStage.STAGE4_3 then --4_3 blue womb
-        ::chooseAgain::
-        local level = Game():GetLevel()
-        local exploredRooms = {}
-        local randomRoom = Game():GetLevel():GetRandomRoomIndex(false, seed)
-        if exploredRooms[randomRoom] == true then
-            seed = seed + 1
-            goto chooseAgain
-        end
-        exploredRooms[randomRoom] = true
-        local roomDescriptor = level:GetRoomByIdx(randomRoom)
-        if roomDescriptor.Data.Shape == RoomShape.ROOMSHAPE_1x1 and roomDescriptor.Data.Type == RoomType.ROOM_DEFAULT then
-            Game():GetLevel():GetRoomByIdx(randomRoom).Data = RoomConfigHolder.GetRoomByStageTypeAndVariant(StbType.SPECIAL_ROOMS, RoomType.ROOM_LIBRARY, 0)
-        else
-            seed = seed + 1
-            goto chooseAgain
+    local game = Game()
+    local level = game:GetLevel()
+    if not game:IsGreedMode() and not level:IsAscent() and not BLACKLISTED_STAGES[level:GetStage()] then
+        if PlayerManager.AnyoneHasTrinket(LIBRARY_CARD) then
+            local roomConfigRoom = RoomConfigHolder.GetRoomByStageTypeAndVariant(StbType.SPECIAL_ROOMS, RoomType.ROOM_LIBRARY, 1, -1)
+            
+            --local rng = Isaac.GetPlayer():GetTrinketRNG(LIBRARY_CARD)
+            local rng = level:GetDevilAngelRoomRNG()
+            local validGridIndexes = level:FindValidRoomPlacementLocations(roomConfigRoom, Dimension.CURRENT, false, false)
+            ::reroll::
+            
+            if validGridIndexes and #validGridIndexes > 0 then
+                local tableIndex = rng:RandomInt(#validGridIndexes) + 1
+                local roomIndex = validGridIndexes[tableIndex]
+                local neighboringRooms = level:GetNeighboringRooms(roomIndex, RoomShape.ROOMSHAPE_1x1, Dimension.CURRENT)
+                for doorSlot, roomDescriptor in pairs(neighboringRooms) do
+                    if roomDescriptor.Data.Type == RoomType.ROOM_SECRET or roomDescriptor.Data.Type == RoomType.ROOM_SUPERSECRET and #neighboringRooms == 1 then
+                        table.remove(validGridIndexes, tableIndex)
+                        goto reroll
+                    else
+                        local room = level:TryPlaceRoom(roomConfigRoom, roomIndex, Dimension.CURRENT, 1, false, false)
+                    end
+                end
+            end
         end
     end
 end

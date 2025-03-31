41f861b392cfdbaf1a90c3e6b28790b46b52ee59
soulspine <soulspime@gmail.com>
2025-03-31 10:15:23 +0200

    change soulbond to work on update instead of room enter

--- scripts/items/Requiem/soulbond.lua
+++ scripts/items/Requiem/soulbond.lua
@@ -1,101 +1,110 @@
 local SOULBOND = Isaac.GetItemIdByName("Soulbond")
 
-local DAMAGE_SHARE_AMOUNT = 0.25
+local DAMAGE_SHARE = 0.25
 
 local BOND_ENTITY_1_COLOR = Color(5, 5, 5, 0.7)
 local BOND_ENTITY_2_COLOR = Color(0.5, 2, 10, 0.7)
+
+local POST_DEATH_GHOST_TYPE =  EntityType.ENTITY_EFFECT
+local POST_DEATH_GHOST_VARIANT = EffectVariant.PURGATORY
+local POST_DEATH_GHOST_SUBTYPE = 1
+
+local TARGET_BONDS_COUNT = function(enemyCount)
+    return math.max(math.floor(enemyCount / 4), 1)
+end
 
 local chainSprite = Sprite()
 chainSprite:Load("gfx/soulbond_chain.anm2", true)
 chainSprite:Play("Idle", true)
 
-local function onRoomEnter()
+local function onUpdate()
     if PlayerManager.AnyoneHasCollectible(SOULBOND) then
-        local player = Isaac.GetPlayer()
-        local data = player:GetData()
-        data.ResouledCorrectEnemies = {}
+        local bondedEnemies = 0
+        local totalEnemies = 0
+        local bindableEnemies = {}
         ---@param entity Entity
         Resouled:IterateOverRoomEntities(function(entity)
-            if entity:IsEnemy() then
-                table.insert(data.ResouledCorrectEnemies, entity)
+            if entity:IsVulnerableEnemy() and entity:IsActiveEnemy() then
+                totalEnemies = totalEnemies + 1
+                local data = entity:GetData()
+                if data.ResouledSoulbond then
+                    bondedEnemies = bondedEnemies + 1
+                elseif not data.ResouledSoulbondBlock then
+                    table.insert(bindableEnemies, EntityRef(entity))
+                end
             end
         end)
-        if #data.ResouledCorrectEnemies > 1 then
-            local rng = RNG()
-            RNG():SetSeed(Game():GetRoom():GetAwardSeed(), 0)
-            local tableIndex = rng:RandomInt(#data.ResouledCorrectEnemies) + 1
-            data.enemy1 = data.ResouledCorrectEnemies[tableIndex]
-            table.remove(data.ResouledCorrectEnemies, tableIndex)
-            Resouled:NewSeed()
-            tableIndex = rng:RandomInt(#data.ResouledCorrectEnemies) + 1
-            data.enemy2 = data.ResouledCorrectEnemies[tableIndex]
-            data.enemy1.Color = BOND_ENTITY_1_COLOR
-            data.enemy2.Color = BOND_ENTITY_2_COLOR
-            data.enemy1:GetData().ResouledSoulBond = {}
-            data.enemy2:GetData().ResouledSoulBond = {}
-            data.enemy1:GetData().ResouledSoulBond.IsBonded = true
-            data.enemy2:GetData().ResouledSoulBond.IsBonded = true
-            data.enemy1:GetData().ResouledSoulBond.Twin = data.enemy2
-            data.enemy2:GetData().ResouledSoulBond.Twin = data.enemy1
-            data.enemy1:GetData().ResouledSoulBond.Color = BOND_ENTITY_1_COLOR
-            data.enemy2:GetData().ResouledSoulBond.Color = BOND_ENTITY_2_COLOR
+        local targetbondedEnemies = TARGET_BONDS_COUNT(totalEnemies) * 2
+
+        if targetbondedEnemies > bondedEnemies and #bindableEnemies > 1 then
+            local enemyIndex1 = math.random(1, #bindableEnemies)
+            local enemy1 = bindableEnemies[enemyIndex1]
+            table.remove(bindableEnemies, enemyIndex1)
+            local enemy2 = bindableEnemies[math.random(1, #bindableEnemies)]
+            local data1 = enemy1.Entity:GetData()
+            local data2 = enemy2.Entity:GetData()
+            data1.ResouledSoulbond = {
+                Other = enemy2,
+                Beam = Beam(chainSprite, 0, false, false)
+            }
+            data1.ResouledSoulbondBlock = true
+            data2.ResouledSoulbond = {
+                Other = enemy1,
+            }
+            data2.ResouledSoulbondBlock = true
         end
     end
 end
-Resouled:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, onRoomEnter)
+Resouled:AddCallback(ModCallbacks.MC_POST_UPDATE, onUpdate)
+
+---@param npc EntityNPC
+local function onNpcUpdate(_, npc)
+    local data = npc:GetData()
+    if data.ResouledSoulbond and npc:IsDead() then
+        ---@type Entity
+        local other = data.ResouledSoulbond.Other.Entity
+        local otherData = other:GetData()
+        if not other:IsDead() then
+            local ghost = Game():Spawn(POST_DEATH_GHOST_TYPE, POST_DEATH_GHOST_VARIANT, npc.Position, Vector.Zero, nil, POST_DEATH_GHOST_SUBTYPE, npc.InitSeed)
+            ghost.Target = other
+            ghost.Color = data.ResouledSoulbond.Beam and BOND_ENTITY_2_COLOR or BOND_ENTITY_1_COLOR
+            data.ResouledSoulbond = nil
+            otherData.ResouledSoulbond = nil
+        end
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, onNpcUpdate)
+
+---@param npc EntityNPC
+---@param offset Vector
+local function onNpcRender(_, npc, offset)
+    local data = npc:GetData()
+    if data.ResouledSoulbond and data.ResouledSoulbond.Beam then
+        ---@type Entity
+        local other = data.ResouledSoulbond.Other.Entity
+        ---@type Beam
+        local beam = data.ResouledSoulbond.Beam
+        beam:Add(Isaac.WorldToScreen(npc.Position), 0)
+        beam:Add(Isaac.WorldToScreen(other.Position), 64)
+        beam:Render()
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_RENDER, onNpcRender)
 
 ---@param entity Entity
 ---@param amount integer
+---@param damageFlags DamageFlag
 ---@param source EntityRef
-local function npcTakeDamage(_, entity, amount, source)
-    local player = Isaac.GetPlayer()
-    local playerData = player:GetData()
+---@param countdown integer
+local function npcTakeDamage(_, entity, amount, damageFlags, source, countdown)
     local data = entity:GetData()
-    if data.ResouledSoulBond then
-        if data.ResouledSoulBond.IsBonded and data.ResouledSoulBond.Twin and not data.ResouledSoulBond.DontTakeDamage then
-            if data.ResouledSoulBond.Twin.HitPoints > 0 then --Game would error without this
-                data.ResouledSoulBond.Twin:GetData().ResouledSoulBond.DontTakeDamage = true
-                data.ResouledSoulBond.Twin:TakeDamage(DAMAGE_SHARE_AMOUNT * amount, DamageFlag.DAMAGE_DEVIL, EntityRef(entity), 0)
-            else
-                data.ResouledSoulBond.Twin = nil
-            end
-
-            if entity.HitPoints - amount <= 0 and data.ResouledSoulBond.Twin then
-                local ghost = Game():Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PURGATORY, entity.Position, Vector.Zero, nil, 1, entity.InitSeed)
-                ghost.Target = data.ResouledSoulBond.Twin
-                ghost.Color = data.ResouledSoulBond.Color
-                playerData.enemy1 = nil
-                playerData.enemy2 = nil
-            end
-        end
-
-        if data.ResouledSoulBond.DontTakeDamage then
-            data.ResouledSoulBond.DontTakeDamage = false
-        end
+    if damageFlags | DamageFlag.DAMAGE_FAKE ~= 0 and data.ResouledSoulbond and not data.ResouledSoulbond.Damage then
+        ---@type Entity
+        local other = data.ResouledSoulbond.Other.Entity
+        local otherData = other:GetData()
+        otherData.ResouledSoulbond.Damage = true
+        other:TakeDamage(amount*DAMAGE_SHARE, damageFlags, source, countdown)
+        otherData.ResouledSoulbond.Damage = nil
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, npcTakeDamage)
-
-local function onRender()
-    if PlayerManager.AnyoneHasCollectible(SOULBOND) then
-        local player = Isaac.GetPlayer()
-        local data = player:GetData()
-        if data.enemy1 and data.enemy2 then
-            local chain
-            if not data.ResouledChain then
-                chain = data.ResouledChain
-            end
-            
-            if not chain then
-                data.ResouledChain = Beam(chainSprite, 0, false, false)
-                chain = data.ResouledChain
-            end
-            
-            chain:Add(Isaac.WorldToScreen(data.enemy1.Position), 0)
-            chain:Add(Isaac.WorldToScreen(data.enemy2.Position), 64)
-
-            chain:Render()
-        end
-    end
-end
-Resouled:AddCallback(ModCallbacks.MC_POST_RENDER, onRender)

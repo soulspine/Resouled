1f64232d482eb805d333d83fd97d02074289f488
soulspine <soulspime@gmail.com>
2025-03-31 22:30:55 +0200

    add active effect to bag o trash, fix a bug where shop pickups would be free or even grand coins

--- scripts/items/FourSoulsV2/bag_o_trash.lua
+++ scripts/items/FourSoulsV2/bag_o_trash.lua
@@ -2,6 +2,8 @@ local BAG_O_TRASH = Isaac.GetItemIdByName("Bag-O-Trash")
 
 local USE_CHARGE = 3
 
+local ACTIVATE_HOLD_DURATION = 90
+
 local KNIFE_VARIANT =  KnifeVariant.BAG_OF_CRAFTING
 
 local BAG_ENTITY_SPRITESHEET = "gfx/effects/bag_o_trash_knife.png"
@@ -28,6 +30,11 @@ local ANIMATION_PLAYER_HIDE_ITEM = "HideItem"
 
 local ANIMATION_PICKUP_PLAYER_PICKUP = "PlayerPickup"
 
+local CHARGEBAR_SPRITE_HEIGHT = 16
+local ANIMATION_CHARGEBAR_EMPTY = "BarEmpty"
+local ANIMATION_CHARGEBAR_FULL = "BarFull"
+local INPUT_IGNORE_DURATION = 4
+
 local ANIMATIONS_BAG_SWING = {
     "Swing",
     "Swing2",
@@ -37,7 +44,23 @@ local ANIMATIONS_BAG_SWING = {
 
 local UNSPECIFIED_WEIGHT = 1
 
-local PICKUP_WHITELSIT = {
+local SPAWN_PICKUPS = {
+    {PickupVariant.PICKUP_COIN, CoinSubType.COIN_LUCKYPENNY},
+    {PickupVariant.PICKUP_COIN, CoinSubType.COIN_STICKYNICKEL},
+    {PickupVariant.PICKUP_COIN, CoinSubType.COIN_DOUBLEPACK},
+    {PickupVariant.PICKUP_KEY, KeySubType.KEY_CHARGED},
+    {PickupVariant.PICKUP_KEY, KeySubType.KEY_DOUBLEPACK},
+    {PickupVariant.PICKUP_BOMB, BombSubType.BOMB_DOUBLEPACK},
+    {PickupVariant.PICKUP_BOMB, BombSubType.BOMB_GOLDENTROLL},
+    {PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_NORMAL},
+    {PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_GOLDEN},
+    {PickupVariant.PICKUP_HEART, HeartSubType.HEART_DOUBLEPACK},
+    {PickupVariant.PICKUP_HEART, HeartSubType.HEART_ROTTEN},
+    {PickupVariant.PICKUP_HEART, HeartSubType.HEART_BONE},
+    {PickupVariant.PICKUP_HEART, HeartSubType.HEART_ETERNAL},
+}
+
+local COLLECT_PICKUP_WHITELSIT = {
     [PickupVariant.PICKUP_BOMB] = true,
     [PickupVariant.PICKUP_COIN] = true,
     [PickupVariant.PICKUP_TAROTCARD] = true,
@@ -89,6 +112,8 @@ local function onActiveUse(_, itemId, rng, player, useFlags, activeSlot, customV
         data.ResouledBagOfTrash = {
             ActiveSlot = activeSlot,
             Swung = false,
+            SpacebarHoldDuration = 0,
+            InputIgnore = 0,
         }
         player:AnimateCollectible(BAG_O_TRASH, ANIMATION_PLAYER_LIFT_ITEM, ANIMATION_PICKUP_PLAYER_PICKUP)
     end 
@@ -98,31 +123,66 @@ Resouled:AddCallback(ModCallbacks.MC_USE_ITEM, onActiveUse, BAG_O_TRASH)
 ---@param player EntityPlayer
 local function onPlayerUpdate(_, player)
     local data = player:GetData()
-    local aimDirection = player:GetAimDirection()
+    
+    if data.ResouledBagOfTrash then
+        local aimDirection = player:GetAimDirection()
+        local activeSlot = data.ResouledBagOfTrash.ActiveSlot
+        local itemDesc = player:GetActiveItemDesc(activeSlot)
+        local game = Game()
+        local room = game:GetRoom()
+    
+        if itemDesc.Item ~= BAG_O_TRASH then
+            data.ResouledBagOfTrash = nil
+            player:AnimateCollectible(BAG_O_TRASH, ANIMATION_PLAYER_HIDE_ITEM, ANIMATION_PICKUP_PLAYER_PICKUP)
+            return
+        end
 
-    if data.ResouledBagOfTrash and not data.ResouledBagOfTrash.Swung and aimDirection:Length() > 0 then
-        data.ResouledBagOfTrash.Swung = true
-        player:AnimateCollectible(BAG_O_TRASH, ANIMATION_PLAYER_HIDE_ITEM, ANIMATION_PICKUP_PLAYER_PICKUP)
-        SFXManager():Play(SFX_SWING)
-        local hitboxOffset = HITBOX_OFFSET*aimDirection
-        local knife = Game():Spawn(EntityType.ENTITY_KNIFE, KNIFE_VARIANT, player.Position + hitboxOffset, Vector.Zero, player, 0, Resouled:NewSeed()):ToKnife()
-        if knife then
-            knife.SizeMulti = HITBOX_SCALE
-            knife.DepthOffset = player.DepthOffset - 1
-            knife.Parent = player
-            knife.SpriteOffset = SPRITE_OFFSET - hitboxOffset/2
-            knife.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
-            local swingAnimation = ANIMATIONS_BAG_SWING[math.random(#ANIMATIONS_BAG_SWING)]
-            local sprite = knife:GetSprite()
-            sprite:ReplaceSpritesheet(0, BAG_ENTITY_SPRITESHEET, true)
-            sprite:Play(swingAnimation, true)
-            sprite:GetLayer(1):SetVisible(true)
-            sprite.PlaybackSpeed = BAG_ENTITY_SPRITE_PLAYBACK_SPEED
-            knife.SpriteRotation = Vector(aimDirection.Y, -aimDirection.X):GetAngleDegrees() -- no idea what logic was behind that, this variation just worked
-            knife:GetData().ResouledBagOfTrash = {
-                HitboxOffset = hitboxOffset,
-                ActiveSlot = data.ResouledBagOfTrash.ActiveSlot,
-            }
+        if not data.ResouledBagOfTrash.Swung and aimDirection:Length() > 0 then -- handling swings
+            data.ResouledBagOfTrash.Swung = true
+            player:AnimateCollectible(BAG_O_TRASH, ANIMATION_PLAYER_HIDE_ITEM, ANIMATION_PICKUP_PLAYER_PICKUP)
+            SFXManager():Play(SFX_SWING)
+            local hitboxOffset = HITBOX_OFFSET*aimDirection
+            local knife = Game():Spawn(EntityType.ENTITY_KNIFE, KNIFE_VARIANT, player.Position + hitboxOffset, Vector.Zero, player, 0, Resouled:NewSeed()):ToKnife()
+            if knife then
+                knife.SizeMulti = HITBOX_SCALE
+                knife.DepthOffset = player.DepthOffset - 1
+                knife.Parent = player
+                knife.SpriteOffset = SPRITE_OFFSET - hitboxOffset/2
+                knife.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
+                local swingAnimation = ANIMATIONS_BAG_SWING[math.random(#ANIMATIONS_BAG_SWING)]
+                local sprite = knife:GetSprite()
+                sprite:ReplaceSpritesheet(0, BAG_ENTITY_SPRITESHEET, true)
+                sprite:Play(swingAnimation, true)
+                sprite:GetLayer(1):SetVisible(true)
+                sprite.PlaybackSpeed = BAG_ENTITY_SPRITE_PLAYBACK_SPEED
+                knife.SpriteRotation = Vector(aimDirection.Y, -aimDirection.X):GetAngleDegrees() -- no idea what logic was behind that, this variation just worked
+                knife:GetData().ResouledBagOfTrash = {
+                    HitboxOffset = hitboxOffset,
+                    ActiveSlot = data.ResouledBagOfTrash.ActiveSlot,
+                }
+            end
+        end
+
+
+
+        if data.ResouledBagOfTrash.SpacebarHoldDuration == ACTIVATE_HOLD_DURATION and itemDesc.VarData >= USE_CHARGE then -- handling spawning
+            player:AnimateCollectible(BAG_O_TRASH, ANIMATION_PLAYER_HIDE_ITEM, ANIMATION_PICKUP_PLAYER_PICKUP)
+            itemDesc.VarData = itemDesc.VarData - USE_CHARGE
+            local rng = player:GetCollectibleRNG(BAG_O_TRASH)
+            local effectNum = rng:RandomInt(3)
+
+            print("EffectNum: " .. effectNum)
+
+            if effectNum == 0 then -- spawn 6 flies
+                player:AddBlueFlies(6, player.Position, nil)
+            elseif effectNum == 1 then -- spawn a trinket
+                game:Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, room:FindFreePickupSpawnPosition(player.Position), Vector.Zero, player, 0, Resouled:NewSeed())
+            elseif effectNum == 2 then
+                local targetPickup = SPAWN_PICKUPS[rng:RandomInt(#SPAWN_PICKUPS) + 1]
+                game:Spawn(EntityType.ENTITY_PICKUP, targetPickup[1], room:FindFreePickupSpawnPosition(player.Position), Vector.Zero, player, targetPickup[2], Resouled:NewSeed())
+            end
+
+            data.ResouledBagOfTrash = nil
         end
     end
 end
@@ -143,7 +203,11 @@ local function onKnifeUpdate(_, knife)
 
         local capsule = knife:GetCollisionCapsule()
         for _, entity in ipairs(Isaac.FindInCapsule(capsule, EntityPartition.PICKUP)) do
-            if entity:ToPickup() and not entity:IsDead() and PICKUP_WHITELSIT[entity.Variant] then
+            local pickup = entity:ToPickup()
+            if pickup and not entity:IsDead() and COLLECT_PICKUP_WHITELSIT[entity.Variant] then
+
+                local price = pickup.Price
+
                 local weight = UNSPECIFIED_WEIGHT
                 if entity.Variant == PickupVariant.PICKUP_COIN then
                     weight = (COIN_WEIGHT[entity.SubType] == nil and 1 or COIN_WEIGHT[entity.SubType])
@@ -161,12 +225,13 @@ local function onKnifeUpdate(_, knife)
                     weight = (HEART_WEIGHT[entity.SubType] == nil and 1 or HEART_WEIGHT[entity.SubType])
                 end
 
-                if weight ~= 0 then
+                if weight ~= 0 and player:GetNumCoins() >= price and price >= 0 then
                     local effect = Game():Spawn(EntityType.ENTITY_EFFECT, VFX_PICKUP_VARIANT, entity.Position, Vector.Zero, nil, 0, Resouled:NewSeed())
                     effect:GetSprite().Scale = VFX_PICKUP_SCALE
                     entity:Remove()
                     local activeSlot = knifeData.ResouledBagOfTrash.ActiveSlot 
                     player:SetActiveVarData(player:GetActiveItemDesc(activeSlot).VarData + weight, activeSlot)
+                    player:AddCoins(-price)
                 end
             end
         end
@@ -187,8 +252,23 @@ Resouled:AddCallback(ModCallbacks.MC_PRE_KNIFE_UPDATE, onKnifeUpdate)
 ---@param inputHook InputHook
 ---@param action ButtonAction
 local function onActionTriggered(_, entity, inputHook, action)
-    if entity and (action == ButtonAction.ACTION_DROP) and entity.Type == EntityType.ENTITY_PLAYER and entity:GetData().ResouledBagOfTrash then
-        return false -- this prevents player from changing active slot when animation is playing so it doesnt screw up counting up contents
+
+    if not entity then return end   
+
+    local data = entity:GetData()
+    if entity.Type == EntityType.ENTITY_PLAYER and data.ResouledBagOfTrash and inputHook == InputHook.IS_ACTION_TRIGGERED then
+        local actionPressed = Input.IsActionPressed(action, entity:ToPlayer().ControllerIndex)
+
+        if data.ResouledBagOfTrash.InputIgnore > 0 and not actionPressed then
+            data.ResouledBagOfTrash.InputIgnore = data.ResouledBagOfTrash.InputIgnore - 1
+        else
+            data.ResouledBagOfTrash.InputIgnore = INPUT_IGNORE_DURATION
+            data.ResouledBagOfTrash.SpacebarHoldDuration = (action == ButtonAction.ACTION_ITEM and actionPressed) and math.min(data.ResouledBagOfTrash.SpacebarHoldDuration + 1, ACTIVATE_HOLD_DURATION) or 0
+        end
+
+        if action == ButtonAction.ACTION_DROP then
+            return false -- this prevents player from changing active slot when animation is playing so it doesnt screw up counting up contents
+        end
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_INPUT_ACTION, onActionTriggered)
@@ -221,6 +301,15 @@ local function onActiveItemRender(_, player, activeSlot, offset, alpha, scale, c
         local textWidth = Isaac.GetTextWidth(text)
         local textPosition = BAG_ITEM_SPRITE.Offset - Vector(textWidth/2, 0)
         Isaac.RenderScaledText(text, textPosition.X, textPosition.Y, scale, scale, 1, 1, 1, alpha)
+
+        local playerData = player:GetData()
+        if (activeSlot == ActiveSlot.SLOT_PRIMARY or activeSlot == ActiveSlot.SLOT_POCKET) and playerData.ResouledBagOfTrash and playerData.ResouledBagOfTrash.SpacebarHoldDuration > 0 and itemDesc.VarData >= USE_CHARGE then
+            local chargeBarSprite = Game():GetHUD():GetChargeBarSprite()
+            chargeBarSprite:Play(ANIMATION_CHARGEBAR_FULL, true)
+            chargeBarSprite.Scale = Vector(scale, scale)
+            local percentageHeld = playerData.ResouledBagOfTrash.SpacebarHoldDuration / ACTIVATE_HOLD_DURATION
+            chargeBarSprite:Render(chargebarOffset, Vector(0, (1-percentageHeld)*CHARGEBAR_SPRITE_HEIGHT)*2)
+        end
         return true
     end
 end

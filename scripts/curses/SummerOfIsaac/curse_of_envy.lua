6f2daeb94e8a26dbb254fc8bfdcaaa8c7a87f11c
soulspine <soulspime@gmail.com>
2025-04-26 22:13:58 +0200

    curse of envy

--- scripts/curses/SummerOfIsaac/curse_of_envy.lua
+++ scripts/curses/SummerOfIsaac/curse_of_envy.lua
@@ -1,7 +1,4 @@
-local SPLIT_CHANCE = 0.05
-local SPRITE_SCALE_DIVIDER = 2
-local HITBOX_SIZE_DIVIDER = 2
-local HEALTH_DIVIDER = 3
+local SPLIT_CHANCE = 0.11
 
 local mapId = Resouled.CursesMapId[Resouled.Curses.CURSE_OF_ENVY]
 
@@ -14,80 +11,19 @@ MinimapAPI:AddMapFlag(
     mapId,
     1
 )
---[[
----@param npc EntityNPC
-local function postNpcDeath(_, npc)
-    if Resouled:CustomCursePresent(Resouled.Curses.CURSE_OF_ENVY) then
-        local randomNum = math.random()
-        if randomNum < SPLIT_CHANCE and npc:IsActiveEnemy(true) and npc:IsEnemy() and not npc:GetData().ResouledIsSplitEnemy then
-            local type = npc.Type
-            local variant = npc.Variant
-            local subtype = npc.SubType
-            
-            local splitEnemy1 = Game():Spawn(type, variant, npc.Position, Vector.Zero, npc, subtype, npc.InitSeed)
-            local splitEnemy2 = Game():Spawn(type, variant, npc.Position, Vector.Zero, npc, subtype, npc.InitSeed)
-            
-            splitEnemy1:GetData().ResouledIsSplitEnemy = true
-            splitEnemy2:GetData().ResouledIsSplitEnemy = true
-            
-            splitEnemy1:ToNPC().Size = npc.Size / HITBOX_SIZE_DIVIDER
-            splitEnemy2:ToNPC().Size = npc.Size / HITBOX_SIZE_DIVIDER
-            
-            splitEnemy1:ToNPC().HitPoints = math.floor(npc.HitPoints / HEALTH_DIVIDER)
-            splitEnemy2:ToNPC().HitPoints = math.floor(npc.HitPoints / HEALTH_DIVIDER)
-            
-            if npc:IsChampion() then
-                local championColor = npc:GetChampionColorIdx()
-                
-                splitEnemy1:ToNPC():MakeChampion(npc.InitSeed, championColor, false)
-                splitEnemy2:ToNPC():MakeChampion(npc.InitSeed, championColor, false)
-            end
 
-            splitEnemy1:ToNPC().SpriteScale = npc.SpriteScale / SPRITE_SCALE_DIVIDER
-            splitEnemy2:ToNPC().SpriteScale = npc.SpriteScale / SPRITE_SCALE_DIVIDER
+---@param entity Entity
+---@param amount integer
+---@param flags DamageFlag
+---@param source EntityRef
+---@param countdown integer
+local function entityTakeDamage(_, entity, amount, flags, source, countdown)
+    local npc = entity:ToNPC()
+    if npc and Resouled:CustomCursePresent(Resouled.Curses.CURSE_OF_ENVY) and entity:IsActiveEnemy() and (entity.HitPoints - amount) <= 0  then
+        local rng = npc:GetDropRNG()
+        if rng:RandomFloat() < SPLIT_CHANCE then
+            npc:TrySplit(0, source, false)
         end
     end
 end
-Resouled:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, postNpcDeath)
-]]
-
----@param npc EntityNPC
-local function npcUpdate(_, npc)
-    if Resouled:CustomCursePresent(Resouled.Curses.CURSE_OF_ENVY) then
-        if npc.HitPoints <= 0 then
-            
-            local randomNum = math.random()
-
-            if randomNum < SPLIT_CHANCE and npc:IsActiveEnemy(true) and npc:IsEnemy() and not npc:GetData().ResouledIsSplitEnemy then
-                local type = npc.Type
-                local variant = npc.Variant
-                local subtype = npc.SubType
-
-                if type == 281 and variant == 0 and subtype == 0 then --swarm flies (can multiply infinitely)
-                    return
-                end
-                
-                local splitEnemy1 = Game():Spawn(type, variant, npc.Position, Vector.Zero, npc, subtype, npc.InitSeed)
-                local splitEnemy2 = Game():Spawn(type, variant, npc.Position, Vector.Zero, npc, subtype, npc.InitSeed)
-                
-                splitEnemy1:GetData().ResouledIsSplitEnemy = true
-                splitEnemy2:GetData().ResouledIsSplitEnemy = true
-                
-                splitEnemy1:ToNPC().Size = npc.Size / HITBOX_SIZE_DIVIDER
-                splitEnemy2:ToNPC().Size = npc.Size / HITBOX_SIZE_DIVIDER
-                
-                splitEnemy1:ToNPC().HitPoints = math.floor(splitEnemy1.HitPoints / HEALTH_DIVIDER)
-                splitEnemy2:ToNPC().HitPoints = math.floor(splitEnemy2.HitPoints / HEALTH_DIVIDER)
-                
-                if npc:IsChampion() then
-                    local championColor = npc:GetChampionColorIdx()
-                    
-                    splitEnemy1:ToNPC():MakeChampion(npc.InitSeed, championColor, false)
-                    splitEnemy2:ToNPC():MakeChampion(npc.InitSeed, championColor, false)
-                    
-                end
-            end
-        end
-    end
-end
-Resouled:AddPriorityCallback(ModCallbacks.MC_NPC_UPDATE, CallbackPriority.IMPORTANT, npcUpdate)
+Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, entityTakeDamage)

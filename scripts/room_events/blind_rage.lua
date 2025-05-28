c39863f588de62dc43fe4625c4fd1853c354b699
Mimi <mojeyt.dc@gmail.com>
2025-05-28 10:59:44 +0200

    fix blind rage

--- scripts/room_events/blind_rage.lua
+++ scripts/room_events/blind_rage.lua
@@ -1,27 +1,8 @@
 local function postRoomClear()
     if Resouled:RoomEventPresent(Resouled.RoomEvents.BLIND_RAGE) then
-        local unclearedRooms = {}
-        ---@param roomIndex integer
-        Resouled.Iterators:IterateOverRooms(function(roomIndex)
-            local room = Game():GetLevel():GetRoomByIdx(roomIndex)
-            if room.Data then
-                if not room.Clear then
-                    table.insert(unclearedRooms, room.GridIndex)
-                end
-            end
-        end)
-        if #unclearedRooms > 0 then
-            local nearestUnclearedRoom = nil
-            for i = 1, #unclearedRooms do
-                if nearestUnclearedRoom == nil then
-                    nearestUnclearedRoom = Resouled:GetGridRoomDistance(Game():GetLevel():GetCurrentRoomIndex(), unclearedRooms[i])
-                end
-                if Resouled:GetGridRoomDistance(Game():GetLevel():GetCurrentRoomIndex(), unclearedRooms[i]) < nearestUnclearedRoom then
-                    nearestUnclearedRoom = unclearedRooms[i]
-                end
-            end
-            Game():GetLevel():ChangeRoom(nearestUnclearedRoom)
-        end 
+        if Game():GetLevel():GetNonCompleteRoomIndex() then 
+            Game():StartRoomTransition(Game():GetLevel():GetNonCompleteRoomIndex(), Direction.NO_DIRECTION, RoomTransitionAnim.TELEPORT)
+        end
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, postRoomClear)

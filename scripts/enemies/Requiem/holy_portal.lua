ce06d2cdcded9476a995feddd811ed642f974fb5
Umimerhimhair <mojeyt.dc@gmail.com>
2025-06-01 14:12:45 +0200

    add all enemy convertion rules to holy portal

--- scripts/enemies/Requiem/holy_portal.lua
+++ scripts/enemies/Requiem/holy_portal.lua
@@ -20,8 +20,60 @@ local function postNpcDeath(_, npc)
     if npc.Type == EntityType.ENTITY_BONY and npc.Variant == 0 then
         ---@param entity Entity
         Resouled.Iterators:IterateOverRoomEntities(function(entity)
-            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then
+            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then --Bony
                 local spawnedEntity = Game():Spawn(EntityType.ENTITY_BONY, 1, entity.Position + Vector(0, 1), Vector(0, 5), nil, 0, entity.InitSeed)
+                spawnedEntity:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
+                spawnedEntity:SetColor(Color(1, 0, 1, 0, 1, 0, 1), 20, 9999, true, true)
+                local randomNum = math.random(1, 3)
+                SFXManager():Play(Isaac.GetSoundIdByName(SPAWN_SFX..tostring(randomNum)), SPAWN_SFX_VOLUME)
+                entity:SetColor(Color(1, 1, 1, 1, 0.25, 0.1, 0.25), 15, 1, true, true)
+            end
+        end)
+    end
+    if npc.Type == EntityType.ENTITY_MULLIGAN then
+        ---@param entity Entity
+        Resouled.Iterators:IterateOverRoomEntities(function(entity)
+            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then --Mulligan
+                local spawnedEntity = Game():Spawn(22, 2, entity.Position + Vector(0, 1), Vector(0, 5), nil, 0, entity.InitSeed)
+                spawnedEntity:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
+                spawnedEntity:SetColor(Color(1, 0, 1, 0, 1, 0, 1), 20, 9999, true, true)
+                local randomNum = math.random(1, 3)
+                SFXManager():Play(Isaac.GetSoundIdByName(SPAWN_SFX..tostring(randomNum)), SPAWN_SFX_VOLUME)
+                entity:SetColor(Color(1, 1, 1, 1, 0.25, 0.1, 0.25), 15, 1, true, true)
+            end
+        end)
+    end
+    if npc.Type == EntityType.ENTITY_LEECH and npc.Variant ~= 2 then
+        ---@param entity Entity
+        Resouled.Iterators:IterateOverRoomEntities(function(entity)
+            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then --Leech
+                local spawnedEntity = Game():Spawn(EntityType.ENTITY_LEECH, 2, entity.Position + Vector(0, 1), Vector(0, 5), nil, 0, entity.InitSeed)
+                spawnedEntity:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
+                spawnedEntity:SetColor(Color(1, 0, 1, 0, 1, 0, 1), 20, 9999, true, true)
+                local randomNum = math.random(1, 3)
+                SFXManager():Play(Isaac.GetSoundIdByName(SPAWN_SFX..tostring(randomNum)), SPAWN_SFX_VOLUME)
+                entity:SetColor(Color(1, 1, 1, 1, 0.25, 0.1, 0.25), 15, 1, true, true)
+            end
+        end)
+    end
+    if npc.Type == EntityType.ENTITY_EYE and npc.Variant ~= 2 then
+        ---@param entity Entity
+        Resouled.Iterators:IterateOverRoomEntities(function(entity)
+            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then --Eye
+                local spawnedEntity = Game():Spawn(EntityType.ENTITY_EYE, 2, entity.Position + Vector(0, 1), Vector(0, 5), nil, 0, entity.InitSeed)
+                spawnedEntity:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
+                spawnedEntity:SetColor(Color(1, 0, 1, 0, 1, 0, 1), 20, 9999, true, true)
+                local randomNum = math.random(1, 3)
+                SFXManager():Play(Isaac.GetSoundIdByName(SPAWN_SFX..tostring(randomNum)), SPAWN_SFX_VOLUME)
+                entity:SetColor(Color(1, 1, 1, 1, 0.25, 0.1, 0.25), 15, 1, true, true)
+            end
+        end)
+    end
+    if npc.Type == EntityType.ENTITY_BABY and npc.Variant ~= 1 and npc.Variant ~= 2 then
+        ---@param entity Entity
+        Resouled.Iterators:IterateOverRoomEntities(function(entity)
+            if entity.Type == HOLY_PORTAL_TYPE and entity.Variant == HOLY_PORTAL_VARIANT and entity.SubType == HOLY_PORTAL_SUBTYPE then --Baby
+                local spawnedEntity = Game():Spawn(EntityType.ENTITY_BABY, 1, entity.Position + Vector(0, 1), Vector(0, 5), nil, 0, entity.InitSeed)
                 spawnedEntity:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
                 spawnedEntity:SetColor(Color(1, 0, 1, 0, 1, 0, 1), 20, 9999, true, true)
                 local randomNum = math.random(1, 3)

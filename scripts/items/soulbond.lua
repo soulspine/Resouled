9a59eaadb1e662806ea6a7bdb45a2abce34451cb
Umimerhimhair <mojeyt.dc@gmail.com>
2025-06-16 21:29:00 +0200

    soulbond

--- scripts/items/soulbond.lua
+++ scripts/items/soulbond.lua
@@ -2,12 +2,9 @@ local SOULBOND = Isaac.GetItemIdByName("Soulbond")
 
 local DAMAGE_SHARE = 0.25
 
-local BOND_ENTITY_1_COLOR = Color(5, 5, 5, 0.7)
-local BOND_ENTITY_2_COLOR = Color(0.5, 2, 10, 0.7)
+local BEAM_SPRITE_MIN = 0
+local BEAM_SPRITE_MAX = 64
 
-local POST_DEATH_GHOST_TYPE =  EntityType.ENTITY_EFFECT
-local POST_DEATH_GHOST_VARIANT = EffectVariant.PURGATORY
-local POST_DEATH_GHOST_SUBTYPE = 1
 
 local TARGET_BONDS_COUNT = function(enemyCount)
     return math.max(math.floor(enemyCount / 4), 1)
@@ -17,6 +14,10 @@ local chainSprite = Sprite()
 chainSprite:Load("gfx/soulbond_chain.anm2", true)
 chainSprite:Play("Idle", true)
 
+local chainLockSprite = Sprite()
+chainLockSprite:Load("gfx/soulbond_chain_2.anm2", true)
+chainLockSprite:Play("Idle", true)
+
 local function onUpdate()
     if PlayerManager.AnyoneHasCollectible(SOULBOND) then
         local bondedEnemies = 0
@@ -24,7 +25,7 @@ local function onUpdate()
         local bindableEnemies = {}
         ---@param entity Entity
         Resouled.Iterators:IterateOverRoomEntities(function(entity)
-            if entity:IsVulnerableEnemy() and entity:IsActiveEnemy() then
+            if entity:IsVulnerableEnemy() and entity:IsActiveEnemy() and entity:IsEnemy() then
                 totalEnemies = totalEnemies + 1
                 local data = entity:GetData()
                 if data.ResouledSoulbond then
@@ -45,7 +46,7 @@ local function onUpdate()
             local data2 = enemy2.Entity:GetData()
             data1.ResouledSoulbond = {
                 Other = enemy2,
-                Beam = Beam(chainSprite, 0, false, false)
+                Beam = Beam(chainSprite, 0, false, false),
             }
             data1.ResouledSoulbondBlock = true
             data2.ResouledSoulbond = {
@@ -65,12 +66,14 @@ local function onNpcUpdate(_, npc)
         local other = data.ResouledSoulbond.Other.Entity
         local otherData = other:GetData()
         if not other:IsDead() then
-            local ghost = Game():Spawn(POST_DEATH_GHOST_TYPE, POST_DEATH_GHOST_VARIANT, npc.Position, Vector.Zero, nil, POST_DEATH_GHOST_SUBTYPE, npc.InitSeed)
-            ghost.Target = other
-            ghost.Color = data.ResouledSoulbond.Beam and BOND_ENTITY_2_COLOR or BOND_ENTITY_1_COLOR
             data.ResouledSoulbond = nil
             otherData.ResouledSoulbond = nil
         end
+    end
+
+    if data.ResouledSoulbond and data.ResouledSoulbond.Other.Entity.HitPoints <= 0 then
+        data.ResouledSoulbond.Other.Entity:GetData().ResouledSoulbond = nil
+        data.ResouledSoulbond = nil
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, onNpcUpdate)
@@ -79,17 +82,49 @@ Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, onNpcUpdate)
 ---@param offset Vector
 local function onNpcRender(_, npc, offset)
     local data = npc:GetData()
-    if data.ResouledSoulbond and data.ResouledSoulbond.Beam then
+    if data.ResouledSoulbond then
         ---@type Entity
         local other = data.ResouledSoulbond.Other.Entity
-        ---@type Beam
-        local beam = data.ResouledSoulbond.Beam
-        beam:Add(Isaac.WorldToScreen(npc.Position), 0)
-        beam:Add(Isaac.WorldToScreen(other.Position), 64)
-        beam:Render()
+        
+        --Get the positions
+        local currentPos = npc.Position
+        local otherPos = other.Position
+        --Apply the sprite offset
+        currentPos = currentPos + npc.SpriteOffset + Vector(0, -(npc.Size/2))
+        otherPos = otherPos + other.SpriteOffset + Vector(0, -(other.Size/2))
+        if data.ResouledSoulbond.Beam then
+            ---@type Beam
+            local beam = data.ResouledSoulbond.Beam
+            
+            local dirVector = (otherPos - currentPos):Normalized() * BEAM_SPRITE_MAX
+            local dirVectorLength = dirVector:Length()
+            local distance = currentPos:Distance(otherPos)
+            local pointPos = currentPos
+            
+            while distance - dirVectorLength > dirVectorLength do
+                beam:Add(Isaac.WorldToScreen(pointPos), BEAM_SPRITE_MIN)
+                beam:Add(Isaac.WorldToScreen(pointPos + dirVector), math.floor(pointPos:Distance(pointPos + dirVector)/2))
+                
+                pointPos = pointPos + dirVector
+                distance = distance - dirVectorLength
+            end
+            
+            beam:Add(Isaac.WorldToScreen(pointPos), BEAM_SPRITE_MIN)
+            beam:Add(Isaac.WorldToScreen(otherPos), math.floor(pointPos:Distance(otherPos)/2))
+            
+            beam:Render()
+        end
+
+        chainLockSprite:Render(Isaac.WorldToScreen(currentPos))
+        chainLockSprite:Render(Isaac.WorldToScreen(otherPos))
+
+    end
+
+    if data.Resouled_NewAlpha then
+        npc:SetColor(Color(npc.Color.R, npc.Color.G, npc.Color.B, data.Resouled_NewAlpha), 2, 1, false, true)
     end
 end
-Resouled:AddCallback(ModCallbacks.MC_PRE_NPC_RENDER, onNpcRender)
+Resouled:AddCallback(ModCallbacks.MC_POST_NPC_RENDER, onNpcRender)
 
 ---@param entity Entity
 ---@param amount integer
@@ -102,9 +137,17 @@ local function npcTakeDamage(_, entity, amount, damageFlags, source, countdown)
         ---@type Entity
         local other = data.ResouledSoulbond.Other.Entity
         local otherData = other:GetData()
-        otherData.ResouledSoulbond.Damage = true
-        other:TakeDamage(amount*DAMAGE_SHARE, damageFlags, source, countdown)
-        otherData.ResouledSoulbond.Damage = nil
+        if otherData.ResouledSoulbond then
+            otherData.ResouledSoulbond.Damage = true
+            other:TakeDamage(amount * DAMAGE_SHARE, damageFlags, source, countdown)
+            otherData.ResouledSoulbond.Damage = nil
+        end
+        
+        local newAlpha = (1 - (entity.HitPoints / entity.MaxHitPoints))/2
+        local newAlphaOther = (1 - (other.HitPoints / other.MaxHitPoints))/2
+
+        data.Resouled_NewAlpha = 1 - newAlpha
+        otherData.Resouled_NewAlpha = 1 - newAlphaOther
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, npcTakeDamage)
@@ -113,6 +156,7 @@ Resouled:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, npcTakeDamage)
 local function postNpcDeath(_, npc)
     local data = npc:GetData()
     if data.ResouledSoulbond then
+        data.ResouledSoulbond.Other.Entity:GetData().ResouledSoulbond = nil
         data.ResouledSoulbond = nil
     end
 end

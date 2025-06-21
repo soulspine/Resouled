7cb67f470137ac6f7c21f168040e5bcf06c13c7f
Umimerhimhair <mojeyt.dc@gmail.com>
2025-06-21 10:07:36 +0200

    fix wood particles randomness

--- scripts/effects/wood_particle.lua
+++ scripts/effects/wood_particle.lua
@@ -4,8 +4,18 @@ local EFFECT_GOLDEN_SUBTYPE = Isaac.GetEntitySubTypeByName("Wood Gold Particle")
 
 ---@param effect EntityEffect
 local function postEffectInit(_, effect)
-    if effect.SubType == (EFFECT_SUBTYPE or EFFECT_GOLDEN_SUBTYPE) then
+    if effect.SubType == EFFECT_SUBTYPE or effect.SubType == EFFECT_GOLDEN_SUBTYPE then
         effect:GetSprite():Play(tostring(math.random(6) + 1), true)
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_POST_EFFECT_INIT, postEffectInit, EFFECT_VARIANT)
+
+---@param effect EntityEffect
+local function postEffectUpdate(_, effect)
+    if effect.SubType == EFFECT_SUBTYPE or effect.SubType == EFFECT_GOLDEN_SUBTYPE then
+        if effect:GetSprite():IsEventTriggered("Death") then
+            effect:Remove()
+        end
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, postEffectUpdate, EFFECT_VARIANT)

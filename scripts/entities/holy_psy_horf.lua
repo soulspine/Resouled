d2c05d4a50561920358eea72f294297e91a5bff9
Umimerhimhair <mojeyt.dc@gmail.com>
2025-06-29 19:51:22 +0200

    fix holy psy horf laser

--- scripts/entities/holy_psy_horf.lua
+++ scripts/entities/holy_psy_horf.lua
@@ -4,6 +4,8 @@ local HOLY_PSY_HORF_SUBTYPE = Isaac.GetEntitySubTypeByName("Holy Psy Horf")
 
 local ATTACK_COOLDOWN = 50
 
+local LASER_TIMEOUT = 22
+
 ---@param npc EntityNPC
 local function postNpcInit(_, npc)
     if npc.Variant == HOLY_PSY_HORF_VARIANT and npc.SubType == HOLY_PSY_HORF_SUBTYPE then
@@ -78,16 +80,16 @@ local function onNpcUpdate(_, npc)
         end
 
         if sprite:IsEventTriggered("Laser") then
-            data.Resouled_Laser = Game():Spawn(EntityType.ENTITY_LASER, LaserVariant.LIGHT_BEAM, npc.Position + Vector(0, 1), Vector.Zero, npc, 0, npc.InitSeed):ToLaser()
-            data.Resouled_Laser.PositionOffset = Vector(0, -40)
-            data.Resouled_Laser:SetTimeout(99999)
+            data.Resouled_Laser = Game():Spawn(EntityType.ENTITY_LASER, LaserVariant.LIGHT_BEAM, npc.Position + Vector(0, 1) + Vector(0, -20), Vector.Zero, npc, 0, npc.InitSeed):ToLaser()
+            data.Resouled_Laser.PositionOffset = Vector(0, -20)
+            data.Resouled_Laser.DepthOffset = npc.DepthOffset + 100
+            data.Resouled_Laser:SetTimeout(LASER_TIMEOUT)
             data.Resouled_Laser:SetColor(Color(0.65, 0.65, 1, 0.75, 0.75, 0.75, 0.75), 99999, 10000, false, false)
             data.Resouled_Laser.Angle = (npc:GetPlayerTarget().Position - npc.Position - npc:GetPlayerTarget().Velocity * 10):GetAngleDegrees()
             data.Resouled_Laser:GetData().Resouled_DamagePlayer = true
         end
 
         if data.Resouled_Laser and sprite:IsEventTriggered("LaserDie") then
-            data.Resouled_Laser:SetTimeout(1)
             data.Resouled_Attack.Cooldown = ATTACK_COOLDOWN
         end
     end

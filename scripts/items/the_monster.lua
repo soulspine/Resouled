086ce53ec1c5d175f907774005b25a806271f5b3
Umimerhimhair <mojeyt.dc@gmail.com>
2025-07-03 01:06:05 +0200

    add the monster rendering

--- scripts/items/the_monster.lua
+++ scripts/items/the_monster.lua
@@ -1,52 +1,52 @@
 local THE_MONSTER = Isaac.GetItemIdByName("The Monster")
-local THE_MONSTER_VARIANT = Isaac.GetEntityVariantByName("The Monster")
 
-local IDLE = "Idle"
-local WALK_DOWN = "WalkDown"
-local WALK_UP = "WalkUp"
-local WALK_LEFT = "WalkLeft"
-local WALK_RIGHT = "WalkRight"
-
-local playerHeadDirectionTranslation = {
-    [-1] = IDLE,
-    [0] = WALK_LEFT,
-    [1] = WALK_UP,
-    [2] = WALK_RIGHT,
-    [3] = WALK_DOWN,
+local Monster = {
+    Sprite = Sprite(),
+    HeadDirTranslation = {[0] = "Left", [1] = "Up", [2] = "Right", [3] = "Down"},
+    OffsetPerMonster = 13,
+    AlphaWhenItemPickup = 0.2,
+    AlphaToSubtractPerMonster = 0, --Make dss option
 }
 
-local HITBOX_SIZE_INCREASE = 4
+Monster.Sprite:Load("gfx/the_monster.anm2", true)
 
 ---@param player EntityPlayer
----@param cacheFlag CacheFlag
-local function onCacheEval(_, player, cacheFlag)
-    if cacheFlag & CacheFlag.CACHE_FAMILIARS then
-        local itemConfigItem = Isaac.GetItemConfig():GetCollectible(THE_MONSTER)
-        player:CheckFamiliar(THE_MONSTER_VARIANT, player:GetCollectibleNum(THE_MONSTER), player:GetCollectibleRNG(THE_MONSTER), itemConfigItem)
+local function onPlayerRender(_, player)
+    if player:HasCollectible(THE_MONSTER) and player:IsVisible() then
+        for i = 0, player:GetCollectibleNum(THE_MONSTER) - 1 do
+            local headDir = player:GetHeadDirection()
+            local playerSprite = player:GetSprite()
+
+            Monster.Sprite.Offset = Vector(0, 0)
+
+            Monster.Sprite.Color.A = 1
+
+            local playerAnimation = playerSprite:GetAnimation()
+            if playerAnimation:find("Item") or playerAnimation:find("Pickup") then
+                local alpha = Monster.AlphaWhenItemPickup * i
+                if alpha > 1 then
+                    alpha = 1
+                end
+                Monster.Sprite.Color.A = 0 + alpha --Make picked up things visible through the monsters
+            end
+
+            Monster.Sprite.Color.A = Monster.Sprite.Color.A - Monster.AlphaToSubtractPerMonster * i --Make the stack less and less visible
+            
+            Monster.Sprite:Play(Monster.HeadDirTranslation[headDir], true) --Rotate correctly
+            
+            Monster.Sprite.Scale = player.SpriteScale * playerSprite:GetLayer("head"):GetSize() --Set correct size
+            
+            Monster.Sprite.Offset.Y = (Monster.Sprite.Offset.Y - Monster.OffsetPerMonster * i) * Monster.Sprite.Scale.Y --Make them stack
+
+            local pos = player.Position + player.SpriteOffset
+            
+            if playerSprite:GetOverlayAnimation():find("Head") and playerSprite:GetOverlayFrame() > 0 then --Player is shooting
+                pos = pos + Vector(0, 2)
+            end
+            local renderPos = Isaac.WorldToScreen(pos)
+            
+            Monster.Sprite:Render(renderPos)
+        end
     end
 end
-Resouled:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, onCacheEval)
-
----@param familiar EntityFamiliar
-local function familiarInit(_, familiar)
-    familiar.Position = familiar.SpawnerEntity.Position
-    familiar.SpawnerEntity.SpriteOffset = Vector(0, -25)
-end
-Resouled:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, familiarInit, THE_MONSTER_VARIANT)
-
----@param familiar EntityFamiliar
-local function npcUpdate(_, familiar)
-    familiar.SpawnerEntity.Size = familiar.SpawnerEntity.Size + HITBOX_SIZE_INCREASE
-
-    local sprite = familiar:GetSprite()
-
-    if familiar.Position ~= familiar.SpawnerEntity.Position then
-        familiar.Position = familiar.SpawnerEntity.Position
-    end
-    familiar.Velocity = familiar.SpawnerEntity.Velocity * 1.5
-
-    local dir = familiar.SpawnerEntity:ToPlayer():GetMovementDirection()
-
-    sprite:Play(playerHeadDirectionTranslation[dir], true)
-end
-Resouled:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, npcUpdate, THE_MONSTER_VARIANT)
+Resouled:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, onPlayerRender)

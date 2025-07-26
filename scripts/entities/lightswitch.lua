aa03ce71c892fa273cce0f22c86bece61eae4405
soulspine <soulspime@gmail.com>
2025-07-26 17:01:16 +0200

    make light switch use pitch black room flag

--- scripts/entities/lightswitch.lua
+++ scripts/entities/lightswitch.lua
@@ -5,20 +5,37 @@ local ENTITY_SUBTYPE = Isaac.GetEntitySubTypeByName(ENTITY_NAME)
 
 local SWITCH_COOLDOWN = 30 -- updates
 
-local DARKEN_STRENGTH = 1
-local DARKEN_TIMEOUT = 5
-
 local ANIMATION_ON = "On"
 local ANIMATION_OFF = "Off"
 local ANIMATION_ON_TO_OFF = "OnToOff"
 local ANIMATION_OFF_TO_ON = "OffToOn"
 
+local ANIMATION_EVENT_ON = "SwitchOn"
+local ANIMATION_EVENT_OFF = "SwitchOff"
+
+local function isPitchBlack()
+    local roomDesc = Game():GetLevel():GetCurrentRoomDesc()
+    return roomDesc.Flags & RoomDescriptor.FLAG_PITCH_BLACK > 0
+end
+
+local function setPitchBlack(state)
+    local roomDesc = Game():GetLevel():GetCurrentRoomDesc()
+    if state then
+        roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_PITCH_BLACK
+    else
+        roomDesc.Flags = roomDesc.Flags & ~RoomDescriptor.FLAG_PITCH_BLACK
+    end
+end
+
 ---@param pickup EntityPickup
 local function onInit(_, pickup)
-    local sprite = pickup:GetSprite()
-    sprite:Play(sprite:GetDefaultAnimation(), true)
-    pickup.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
-    pickup.GridCollisionClass = EntityGridCollisionClass.GRIDCOLL_WALLS
+    if pickup.SubType == ENTITY_SUBTYPE then
+        local sprite = pickup:GetSprite()
+        local targetAnimation = isPitchBlack() and ANIMATION_OFF or ANIMATION_ON
+        sprite:Play(targetAnimation, true)
+        pickup.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
+        pickup.GridCollisionClass = EntityGridCollisionClass.GRIDCOLL_WALLS
+    end
 end
 Resouled:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, onInit, ENTITY_VARIANT)
 
@@ -35,6 +52,17 @@ local function onUpdate(_, pickup)
         end
 
         local sprite = pickup:GetSprite()
+        local isPitchBlack = isPitchBlack()
+        local transitioning = sprite:IsPlaying(ANIMATION_ON_TO_OFF) or sprite:IsPlaying(ANIMATION_OFF_TO_ON)
+
+        -- syncing other light switches
+        if isPitchBlack and sprite:IsPlaying(ANIMATION_ON) then
+            sprite:Play(ANIMATION_ON_TO_OFF, true)
+            data.RESOULED_LIGHTSWITCH_IGNORE_NEXT_EVENT = true
+        elseif not isPitchBlack and sprite:IsPlaying(ANIMATION_OFF) then
+            sprite:Play(ANIMATION_OFF_TO_ON, true)
+            data.RESOULED_LIGHTSWITCH_IGNORE_NEXT_EVENT = true
+        end
 
         if sprite:IsFinished(ANIMATION_ON_TO_OFF) then
             sprite:Play(ANIMATION_OFF, true)
@@ -42,8 +70,15 @@ local function onUpdate(_, pickup)
             sprite:Play(ANIMATION_ON, true)
         end
 
-        if sprite:IsPlaying(ANIMATION_OFF) then
-            Game():Darken(DARKEN_STRENGTH, DARKEN_TIMEOUT)
+        local eventTriggered = sprite:IsEventTriggered(ANIMATION_EVENT_ON) or
+            sprite:IsEventTriggered(ANIMATION_EVENT_OFF)
+
+        if eventTriggered then
+            if data.RESOULED_LIGHTSWITCH_IGNORE_NEXT_EVENT then
+                data.RESOULED_LIGHTSWITCH_IGNORE_NEXT_EVENT = nil
+            else
+                setPitchBlack(not isPitchBlack)
+            end
         end
     end
 end
@@ -53,18 +88,23 @@ Resouled:AddCallback(ModCallbacks.MC_PRE_PICKUP_UPDATE, onUpdate, ENTITY_VARIANT
 ---@param collider Entity
 ---@param low boolean
 local function onCollision(_, pickup, collider, low)
-    local data = pickup:GetData()
-    local validNpc = collider:ToNPC() and collider:ToNPC():IsFlying()
-    if not data.RESOULED_LIGHTSWITCH_COOLDOWN and (collider:ToPlayer() or validNpc) then
-        local sprite = pickup:GetSprite()
+    if pickup.SubType == ENTITY_SUBTYPE then
+        local data = pickup:GetData()
+        local validNpc = collider:ToNPC() and not collider:ToNPC():IsFlying()
+        if pickup.SubType == ENTITY_SUBTYPE
+            and not data.RESOULED_LIGHTSWITCH_COOLDOWN
+            and (collider:ToPlayer() or validNpc)
+        then
+            local sprite = pickup:GetSprite()
 
-        if sprite:IsPlaying(ANIMATION_ON) then
-            sprite:Play(ANIMATION_ON_TO_OFF, true)
-        else
-            sprite:Play(ANIMATION_OFF_TO_ON, true)
+            if sprite:IsPlaying(ANIMATION_ON) then
+                sprite:Play(ANIMATION_ON_TO_OFF, true)
+            else
+                sprite:Play(ANIMATION_OFF_TO_ON, true)
+            end
+            data.RESOULED_LIGHTSWITCH_COOLDOWN = SWITCH_COOLDOWN
         end
-        data.RESOULED_LIGHTSWITCH_COOLDOWN = SWITCH_COOLDOWN
+        return true -- prevent collision
     end
-    return true -- cancel collision
 end
 Resouled:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, onCollision, ENTITY_VARIANT)

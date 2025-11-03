ef245971e5cdac36eb3396f6f63b15611fa10c5f
Mimi <mojeyt.dc@gmail.com>
2025-11-03 11:23:38 +0100

    add cursed mulliboom

---
+++ scripts/entities/cursed_mulliboom.lua
@@ -0,0 +1,19 @@
+local ID = Isaac.GetEntityTypeByName("Cursed Mulliboom")
+local VARIANT = Isaac.GetEntityVariantByName("Cursed Mulliboom")
+local SUBTYPE = Isaac.GetEntitySubTypeByName("Cursed Mulliboom")
+
+local EXPLOSION_DAMAGE = 60
+local BLAST_RADIUS = 87
+
+---@param npc EntityNPC
+local function npcDeath(_, npc)
+    if npc.Variant == VARIANT and npc.SubType == SUBTYPE then
+        local npcs = Isaac.FindInRadius(npc.Position, BLAST_RADIUS, EntityPartition.ENEMY)
+        for _, npc2 in pairs(npcs) do
+            if npc2:IsEnemy() and npc2:IsActiveEnemy() and npc2:IsVulnerableEnemy() and not npc2:IsDead() and npc2.HitPoints > 0 then
+                Game():BombExplosionEffects(npc2.Position, EXPLOSION_DAMAGE)
+            end
+        end
+    end
+end
+Resouled:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, npcDeath, ID)

f1eec02366cf036a1f6446841713c47cf5c2ca2e
Mimi <mojeyt.dc@gmail.com>
2025-11-05 14:00:53 +0100

    fix j&e afterlife entering giving error bug

--- scripts/other/stat_buffs.lua
+++ scripts/other/stat_buffs.lua
@@ -2,25 +2,27 @@ local function postGameEnd()
     ---@param player EntityPlayer
     Resouled.Iterators:IterateOverPlayers(function(player)
         local startingStats = Resouled:GetPlayerStartingStats(player)
-
-        if startingStats.Damage > player.Damage then
-            Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.STRENGTH)
-        end
-
-        if startingStats.Tears < player.MaxFireDelay then
-            Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.SADNESS)
-        end
-
-        if startingStats.Speed > player.MoveSpeed then
-            Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.AGILITY)
-        end
-
-        if startingStats.Luck > player.Luck then
-            Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.FORTUNE)
-        end
-
-        if startingStats.Range > player.TearRange then
-            Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.SIGHT)
+        if startingStats then
+            
+            if startingStats.Damage and startingStats.Damage > player.Damage then
+                Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.STRENGTH)
+            end
+            
+            if startingStats.Tears and startingStats.Tears < player.MaxFireDelay then
+                Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.SADNESS)
+            end
+            
+            if startingStats.Speed and startingStats.Speed > player.MoveSpeed then
+                Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.AGILITY)
+            end
+            
+            if startingStats.Luck and startingStats.Luck > player.Luck then
+                Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.FORTUNE)
+            end
+            
+            if startingStats.Range and startingStats.Range > player.TearRange then
+                Resouled.AfterlifeShop:AddSpecialBuffToSpawn(Resouled.Buffs.SIGHT)
+            end
         end
     end)
 end

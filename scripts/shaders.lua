c293605644032aa24e615e8f83ac9540e449b57e
Mimi <mojeyt.dc@gmail.com>
2025-11-17 15:01:50 +0100

    magic marker shader sharper

--- scripts/shaders.lua
+++ scripts/shaders.lua
@@ -2,16 +2,17 @@ local x = 0
 local maxAuraSize = 0 --20
 
 Resouled:AddCallback(ModCallbacks.MC_GET_SHADER_PARAMS, function(_, shaderName)
+    local player = Isaac.GetPlayer()
+    local position = Isaac.WorldToScreen(player.Position + Vector(0, -player.Size * 1.5))
     if shaderName == 'ResouledBlankCanvas' then
-        local player = Isaac.GetPlayer()
-        local position = Isaac.WorldToScreen(player.Position + Vector(0, -player.Size * 1.5))
         x = math.min(x + 1, (maxAuraSize + x)/2)
         return {
             PointPos = {
                 position.X,
                 position.Y
             },
-            AreaSize = math.log(x) * x
+            AreaSize = math.log(x) * x,
+            Smoothing = 3
         }
     end
 end)

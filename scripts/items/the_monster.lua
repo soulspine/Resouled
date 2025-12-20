d2af9d12cbff5799da752b65f8f9bc56071744d7
Mimi <mojeyt.dc@gmail.com>
2025-12-20 22:32:38 +0100

    Add friendly sack costume and monster tongue effects; refactor related scripts

--- scripts/items/the_monster.lua
+++ scripts/items/the_monster.lua
@@ -10,17 +10,6 @@ local Monster = {
     WobbleEnabled = true,
 }
 
-local Tongue = {
-    Sprite = Sprite(),
-    SpriteLayer = 0,
-    MaxLength = 10,
-    Animations = {
-        Start = "Start",
-        Part = "Part",
-        End = "End",
-    }
-}
-
 local Chargebar = {
     Sprite = Sprite(),
     Offset = Vector(-30, -45),
@@ -51,7 +40,6 @@ local Chargebar = {
 }
 
 Monster.Sprite:Load("gfx_resouled/the_monster.anm2", true)
-Tongue.Sprite:Load("gfx_resouled/the_monster_tongue.anm2", true)
 Chargebar.Sprite:Load("gfx_resouled/chargebar.anm2", true)
 Chargebar.Sprite:ReplaceSpritesheet(0, "gfx_resouled/ui/monster_chargebar.png", true)
 
@@ -176,23 +164,6 @@ end
 Resouled:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, chargebarRender)
 
 ---@param player EntityPlayer
-local function tongueBeamsRender(_, player)
-    local playerData = player:GetData()
-    if not playerData.RESOULED__MONSTER_BEAMS then return end
-
-    ---@type Beam[]
-    local beamsContainer = playerData.RESOULED__MONSTER_BEAMS
-
-    for i, beam in ipairs(beamsContainer) do
-        ---@type Vector
-        local mouthPos = playerData.RESOULED__MONSTER_MOUTH_POS[i]
-
-        local pointsNum = math.ceil(beam:GetPoints() / 2)
-    end
-end
-Resouled:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, tongueBeamsRender)
-
----@param player EntityPlayer
 local function chargebarPlayerUpdate(_, player)
     local itemCount = player:GetCollectibleNum(ITEM)
 
@@ -205,14 +176,7 @@ local function chargebarPlayerUpdate(_, player)
     if not Resouled:IsPlayerShooting(player) or playerAnimation:find("Item") or playerAnimation:find("Pickup") then
         if data.RESOULED__MONSTER_CHARGEBAR and data.RESOULED__MONSTER_CHARGEBAR.Remaining == 0 then
             -- shoot here
-            data.RESOULED__MONSTER_BEAMS = {
-                Beams = {}
-            }
 
-            for _, _ in ipairs(data.RESOULED__MONSTER_MOUTH_POS) do
-                local beam = Beam(Tongue.Sprite, Tongue.SpriteLayer, false, false)
-                table.insert(data.RESOULED__MONSTER_BEAMS.Beams, beam)
-            end
         end
 
         data.RESOULED__MONSTER_CHARGEBAR = nil
@@ -231,3 +195,85 @@ local function chargebarPlayerUpdate(_, player)
     end
 end
 Resouled:AddCallback(ModCallbacks.MC_PRE_PLAYER_UPDATE, chargebarPlayerUpdate)
+
+
+local tongueSprite = Sprite()
+tongueSprite:Load("gfx_resouled/effects/monster_tongue.anm2", true)
+tongueSprite:Play("Idle", true)
+local tongueBeam = Beam(tongueSprite, 0, false, false)
+local POINT_LENGTH = 8
+local POINT_COUNT = 128/8
+
+local points = {}
+
+local function postRender()
+    local pos1 = Isaac.WorldToScreen(Input.GetMousePosition(true))
+    local pos2 = Isaac.WorldToScreen(Isaac.GetPlayer().Position)
+
+    local rotation = (pos2 - pos1):GetAngleDegrees()
+
+    local startPos = Vector(0, 1)
+    local endPos = Vector((pos2 - pos1):Rotated(-rotation).X, 1)
+
+    if not points[1] then
+        for i = 1, POINT_COUNT do
+            points[i] = {
+                Pos = startPos,
+                Vel = Vector.Zero
+            }
+        end
+    end
+
+    points[1].Pos = startPos
+    points[POINT_COUNT].Vel = (endPos - points[POINT_COUNT].Pos):Resized(points[POINT_COUNT].Pos:Distance(endPos)/10)
+
+    for i = 1, POINT_COUNT do
+        local point = points[i]
+        local otherPoint = points[i + 1]
+        if i ~= 1 and otherPoint then
+            if point.Pos.Y > 0 then
+                point.Vel = point.Vel + Vector(0, 1)
+            end
+
+            if point.Pos:Distance(otherPoint.Pos) > POINT_LENGTH then
+                point.Vel = point.Vel + (otherPoint.Pos - point.Pos):Resized(point.Pos:Distance(otherPoint.Pos)/2)
+            end
+        end
+
+        point.Pos = point.Pos + point.Vel/2
+        point.Vel = point.Vel * 0.975
+    end
+
+    for i = 0, POINT_COUNT - 1 do
+        local point = points[POINT_COUNT - i]
+        local otherPoint = points[POINT_COUNT - (i + 1)]
+        if i ~= 0 and otherPoint then
+            if point.Pos.Y > 0 then
+                point.Vel = point.Vel + Vector(0, 1)
+            end
+
+            if point.Pos:Distance(otherPoint.Pos) > POINT_LENGTH then
+                point.Vel = point.Vel + (otherPoint.Pos - point.Pos):Resized(point.Pos:Distance(otherPoint.Pos)/2)
+            end
+        end
+
+        point.Pos = point.Pos + point.Vel/2
+        point.Vel = point.Vel * 0.975
+    end
+
+    local yStep = pos2 - pos1
+
+    for i = 1, POINT_COUNT - 1 do
+        local point = points[i]
+        local otherPoint = points[i + 1]
+
+        local renderPos1 = Vector(Vector(point.Pos.X, 0):Rotated(rotation).X, point.Pos.Y/5 + yStep.Y * (i - 1)/POINT_COUNT)
+        local renderPos2 = Vector(Vector(otherPoint.Pos.X, 0):Rotated(rotation).X, otherPoint.Pos.Y/5 + yStep.Y * i/POINT_COUNT)
+            
+        tongueBeam:Add(pos1 + renderPos1, POINT_LENGTH * (i - 1))
+        tongueBeam:Add(pos1 + renderPos2, POINT_LENGTH * i)
+    end
+
+    tongueBeam:Render(true)
+end
+--Resouled:AddCallback(ModCallbacks.MC_POST_RENDER, postRender)
